97bae9c9c841f5a0595f2ba9c07a8d806e4ebbe9
Fabien <fabien@dossierfacile.fr>
2022-04-21 14:15:25 +0200

    feat: crop identification image

---
+++ dossierfacile-pdf-generator/src/main/java/fr/dossierfacile/api/pdfgenerator/service/templates/BOIdentificationPdfDocumentTemplate.java
@@ -0,0 +1,118 @@
+package fr.dossierfacile.api.pdfgenerator.service.templates;
+
+import fr.dossierfacile.api.pdfgenerator.model.FileInputStream;
+import fr.dossierfacile.api.pdfgenerator.service.interfaces.PdfTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.util.List;
+
+@Service
+@Slf4j
+@Qualifier("boIdentificationPdfDocumentTemplate")
+public class BOIdentificationPdfDocumentTemplate extends BOPdfDocumentTemplate implements PdfTemplate<List<FileInputStream>> {
+
+    public BOIdentificationPdfDocumentTemplate(MessageSource messageSource) {
+        super(messageSource);
+    }
+
+    /**
+     * create grid sample containing a small matrix computed from a source image
+     */
+    private int[][] createGridSample(float scale, BufferedImage sourceImage) {
+        int gridWidth = (int) (sourceImage.getWidth() * scale);
+        int gridHeight = (int) (sourceImage.getHeight() * scale);
+
+        // convert image to small resolution image
+        BufferedImage image = new BufferedImage(gridWidth, gridHeight, BufferedImage.TYPE_INT_RGB);
+
+        AffineTransform affineTransform = new AffineTransform();
+        affineTransform.scale(scale, scale);
+
+        Graphics2D g = image.createGraphics();
+        g.setColor(Color.WHITE);
+        g.drawImage(sourceImage, affineTransform, null);
+        g.dispose();
+
+        // compute grid
+        int[][] grid = new int[gridWidth][gridHeight];
+
+        Raster raster = image.getRaster();
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++)
+                grid[x][y] = raster.getSample(x, y, 0);
+        }
+        return grid;
+    }
+
+    /**
+     * returns true if specified column if blank
+     */
+    private boolean isBlankColumn(int[][] grid, int col) {
+        for (int row = 0; row < grid[col].length; row++) {
+            if (grid[col][row] < 250) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * returns true if specified row if blank
+     */
+    private boolean isBlankLine(int[][] grid, int row) {
+        for (int col = 0; col < grid.length; col++) {
+            if (grid[col][row] < 250) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * crop blank border on both axis
+     */
+    @Override
+    protected BufferedImage smartCrop(BufferedImage image) {
+
+        // then create a grid
+        float scale = 48 / (float) image.getWidth();
+        int[][] gridSample = createGridSample(scale, image);
+
+        // crop X
+        int col = 0;
+        while (col < gridSample.length && isBlankColumn(gridSample, col)) {
+            col++;
+        }
+        int maxCol = gridSample.length - 1;
+        while (maxCol > col && isBlankColumn(gridSample, maxCol)) {
+            maxCol--;
+        }
+        // crop Y
+        int row = 0;
+        while (row < gridSample[0].length && isBlankLine(gridSample, row)) {
+            row++;
+        }
+        int maxRow = gridSample[0].length - 1;
+        while (maxRow > row && isBlankLine(gridSample, maxRow)) {
+            maxRow--;
+        }
+        maxRow = Math.min(maxRow + 2, gridSample[0].length);
+        maxCol = Math.min(maxCol + 2, gridSample.length);
+
+        int x = (int) (col / scale);
+        int y = (int) (row / scale);
+        int width = (int) (image.getWidth() - x - ((gridSample.length - maxCol) / scale));
+        int height = (int) (image.getHeight() - y - ((gridSample[0].length - maxRow) / scale));
+
+        return image.getSubimage(x, y, width, height);
+    }
+
+}
+
---
+++ dossierfacile-pdf-generator/src/test/java/fr/dossierfacile/api/pdfgenerator/service/templates/IdentificationBOPdfDocumentTemplateTest.java
@@ -0,0 +1,102 @@
+package fr.dossierfacile.api.pdfgenerator.service.templates;
+
+import fr.dossierfacile.api.pdfgenerator.model.FileInputStream;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.MessageSource;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.any;
+
+// Used for manual testing
+@Disabled
+@ExtendWith(MockitoExtension.class)
+public class IdentificationBOPdfDocumentTemplateTest {
+
+    @Mock
+    MessageSource messageSource;
+    @InjectMocks
+    BOIdentificationPdfDocumentTemplate boPdfDocumentTemplate;
+
+    @BeforeEach
+    void init() {
+        Mockito.when(messageSource.getMessage(any(),any(),any(),any() )).thenReturn(BOPdfDocumentTemplate.DEFAULT_WATERMARK);
+    }
+
+    @DisplayName("Check if the render is correctly generated and with enough quality from landscape ID pdf")
+    @Test
+    public void check_render_with_landscape_pdf() throws IOException {
+        InputStream is = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("landscapeid.pdf");
+
+        FileInputStream data = FileInputStream
+                .builder()
+                .extension("pdf")
+                .inputStream(is)
+                .build();
+        InputStream is2 = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("landscapeid2.pdf");
+        FileInputStream data2 = FileInputStream
+                .builder()
+                .extension("pdf")
+                .inputStream(is2)
+                .build();
+
+        File resultFile = new File("target/resultLandscape.pdf");
+        resultFile.createNewFile();
+
+        byte[] bytes = IOUtils.toByteArray(boPdfDocumentTemplate.render(Arrays.asList(data, data2)));
+
+        FileOutputStream w = new FileOutputStream(resultFile);
+        w.write(bytes);
+    }
+
+    @DisplayName("Check if the render is correctly generated from all type textual pdf, pdf, image")
+    @Test
+    public void check_render_with_all_type() throws IOException {
+        InputStream is = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("CNI.pdf");
+        InputStream isJPG = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("CNI.jpg");
+        InputStream isTextPdf = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("landscapeid.pdf");
+        InputStream isOpen = IdentificationBOPdfDocumentTemplateTest.class.getClassLoader().getResourceAsStream("CNI.pdf");
+
+        FileInputStream data = FileInputStream
+                .builder()
+                .extension("pdf")
+                .inputStream(is)
+                .build();
+        FileInputStream data2 = FileInputStream
+                .builder()
+                .extension("jpg")
+                .inputStream(isJPG)
+                .build();
+        FileInputStream data3 = FileInputStream
+                .builder()
+                .extension("pdf")
+                .inputStream(isTextPdf)
+                .build();
+        FileInputStream data4 = FileInputStream
+                .builder()
+                .extension("pdf")
+                .inputStream(isOpen)
+                .build();
+
+        File resultFile = new File("target/resultFullTypeTestPdf.pdf");
+        resultFile.createNewFile();
+
+        byte[] bytes = IOUtils.toByteArray(boPdfDocumentTemplate.render(Arrays.asList(data, data2, data3, data4)));
+
+        FileOutputStream w = new FileOutputStream(resultFile);
+        w.write(bytes);
+    }
+}
