2078218ef8bab28a6a43fbc1590424ece1889f48
Matthieu Bollot <m.bollot@code-troopers.com>
2022-05-02 11:14:09 +0200

    feat: 🎸 Add proxy to search address

--- dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/property/PropertyController.java
+++ dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/property/PropertyController.java
@@ -7,15 +7,27 @@ import fr.dossierfacile.api.dossierfacileapiowner.user.OwnerModel;
 import fr.dossierfacile.common.entity.Owner;
 import fr.dossierfacile.common.enums.LogType;
 import lombok.AllArgsConstructor;
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.utils.URIBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -48,6 +60,23 @@ public class PropertyController {
         propertyService.delete(id);
         Owner owner = authenticationFacade.getOwner();
         return ok(ownerMapper.toOwnerModel(owner));
+    }
+
+    @GetMapping(value = "/listAddresses/{query}")
+    public ResponseEntity<String> getAddresses(@PathVariable String query) throws HttpResponseException {
+        try {
+            URI uri = new URIBuilder("https://api-adresse.data.gouv.fr/search/").addParameter("q", query).addParameter("limit", "15").build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(uri)
+                    .GET()
+                    .build();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return ok(response.body());
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        throw new HttpResponseException(404, "No result");
     }
 
 }
