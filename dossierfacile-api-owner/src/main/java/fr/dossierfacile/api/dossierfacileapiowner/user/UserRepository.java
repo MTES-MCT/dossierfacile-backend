3aa84c7af719ec039e24267eed98c6fa71110e58
Matthieu Bollot <m.bollot@code-troopers.com>
2022-06-02 16:17:45 +0200

    feat: 🎸 Use tenant or owner instead of user to find by mail

--- dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/user/UserRepository.java
+++ dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/user/UserRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findOneByEmailAndEnabledTrue(String toLowerCase);
-
-    Optional<User> findByEmail(String email);
-
 }
--- dossierfacile-migrate-user/src/main/java/fr/dossierfacile/migrate/user/repository/OwnerRepository.java
+++ dossierfacile-migrate-user/src/main/java/fr/dossierfacile/migrate/user/repository/OwnerRepository.java
@@ -1,12 +1,13 @@
 package fr.dossierfacile.migrate.user.repository;
 
+import fr.dossierfacile.common.entity.Owner;
 import fr.dossierfacile.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OwnerRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
+    Optional<Owner> findByEmail(String email);
 
 }
