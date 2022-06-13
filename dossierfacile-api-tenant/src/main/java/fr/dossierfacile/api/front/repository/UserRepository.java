74d1a008728e9366ddd20c13dfb14c342560ccb1
Matthieu Bollot <m.bollot@code-troopers.com>
2022-06-13 14:10:51 +0200

    fix(tenant): 🐛 Add import to fix build

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/repository/UserRepository.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/repository/UserRepository.java
@@ -3,6 +3,8 @@ package fr.dossierfacile.api.front.repository;
 import fr.dossierfacile.common.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findOneByEmailAndEnabledTrue(String toLowerCase);
 
