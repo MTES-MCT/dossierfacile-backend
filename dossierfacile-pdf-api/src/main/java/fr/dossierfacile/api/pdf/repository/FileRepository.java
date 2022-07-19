06a72e080a1b6722b0cc8f21d74a5b4dde0d5c15
Harlow <harlow@dossierfacile.fr>
2022-07-19 11:03:43 -0400

    Deleting old package

--- dossierfacile-pdf-api/src/main/java/fr/dossierfacile/api/pdf/repository/FileRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.dossierfacile.api.pdf.repository;
-
-import fr.dossierfacile.common.entity.File;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FileRepository extends JpaRepository<File, Long> {
-}
