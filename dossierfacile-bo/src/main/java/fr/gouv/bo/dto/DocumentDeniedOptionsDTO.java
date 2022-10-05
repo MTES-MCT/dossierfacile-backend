7325800678efa97d9613cfe66f168362c6446210
Juliette <juliette@dossierfacile.fr>
2022-10-05 16:16:34 +0200

    feat: Add page to edit document denied options

---
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/dto/DocumentDeniedOptionsDTO.java
@@ -0,0 +1,8 @@
+package fr.gouv.bo.dto;
+
+import lombok.Data;
+
+@Data
+public class DocumentDeniedOptionsDTO {
+    private final String messageValue;
+}
--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/DocumentDeniedOptionsRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/DocumentDeniedOptionsRepository.java
@@ -2,11 +2,16 @@ package fr.gouv.bo.repository;
 
 import fr.dossierfacile.common.entity.DocumentDeniedOptions;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface DocumentDeniedOptionsRepository extends JpaRepository<DocumentDeniedOptions, Integer> {
 
     List<DocumentDeniedOptions> findAllByDocumentSubCategoryAndDocumentUserTypeOrderByCode(DocumentSubCategory documentSubCategory, String documentUserType);
+
+    List<DocumentDeniedOptions> findAllByDocumentSubCategory(DocumentSubCategory documentSubCategory);
+
+    DocumentDeniedOptions findByCode(String code);
 
 }
