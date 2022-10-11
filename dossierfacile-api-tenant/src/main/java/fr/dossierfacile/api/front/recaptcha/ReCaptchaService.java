19e34c3fbba8ecf7cb356587be473f60d149bf95
Fabien <fabien@dossierfacile.fr>
2022-10-11 11:28:54 +0200

    recaptcha only if secret is set

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/recaptcha/ReCaptchaService.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/recaptcha/ReCaptchaService.java
@@ -2,6 +2,8 @@ package fr.dossierfacile.api.front.recaptcha;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestOperations;
 
