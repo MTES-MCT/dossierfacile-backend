08ec344067cb389cc0d8a6c4943c1aee7bb884d0
Matthieu Bollot <m.bollot@code-troopers.com>
2022-10-14 16:29:20 +0200

    feat(watermark): 🎸 Add cors config

--- dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/configuration/SwaggerConfig.java
+++ dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/configuration/SwaggerConfig.java
@@ -1,7 +1,6 @@
 package fr.dossierfacile.api.pdf.configuration;
 
 
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,6 +9,8 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
