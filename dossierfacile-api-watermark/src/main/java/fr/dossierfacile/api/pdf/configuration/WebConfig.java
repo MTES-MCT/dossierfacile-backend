768cf840075e9cff3225f515c777302266730f42
Matthieu Bollot <m.bollot@code-troopers.com>
2022-10-14 16:34:01 +0200

    feat(watermark): 🎸 Add cors config (#215)

---
+++ dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/configuration/WebConfig.java
@@ -0,0 +1,22 @@
+package fr.dossierfacile.api.pdf.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("https://dossierfacile-filigrane-prod-front.osc-secnum-fr1.scalingo.io").allowedOrigins("http://localhost:3000").allowedMethods("*");
+            }
+        };
+    }
+}
