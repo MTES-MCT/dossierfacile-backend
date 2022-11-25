0eae39b78a9624c7b50a082b1f4322338e47530d
Fabien <fabien@dossierfacile.fr>
2022-11-25 17:37:30 +0100

    feat: manage messages by tenant

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/controller/MessageController.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/controller/MessageController.java
@@ -4,15 +4,16 @@ import fr.dossierfacile.api.front.form.MessageForm;
 import fr.dossierfacile.api.front.model.MessageModel;
 import fr.dossierfacile.api.front.security.interfaces.AuthenticationFacade;
 import fr.dossierfacile.api.front.service.interfaces.MessageService;
-import fr.dossierfacile.common.entity.Tenant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,15 +25,16 @@ public class MessageController {
     private final MessageService messageService;
     private final AuthenticationFacade authenticationFacade;
 
+    @PreAuthorize("hasPermissionOnTenant(#tenantId)")
     @GetMapping("")
-    public ResponseEntity<List<MessageModel>> all() {
-        Tenant tenant = authenticationFacade.getLoggedTenant();
-        return ResponseEntity.ok(messageService.findAll(tenant));
+    public ResponseEntity<List<MessageModel>> all(@RequestParam(required = false) Long tenantId) {
+        return ResponseEntity.ok(messageService.findAll(authenticationFacade.getTenant(tenantId)));
     }
 
+    @PreAuthorize("hasPermissionOnTenant(#messageForm.tenantId)")
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<MessageModel> create(@Validated @RequestBody MessageForm messageForm) {
-        var tenant = authenticationFacade.getLoggedTenant();
+        var tenant = authenticationFacade.getTenant(messageForm.getTenantId());
         messageService.updateStatusOfDeniedDocuments(tenant);
         return ResponseEntity.ok(messageService.create(tenant, messageForm, false));
     }
