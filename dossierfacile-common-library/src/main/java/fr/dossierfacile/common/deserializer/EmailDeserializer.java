f9fb2a2fa740b6718081921d2ba185235c2ff343
Fabien <fabien@dossierfacile.fr>
2022-12-16 12:05:46 +0100

    fix: empty string on email is deserialize as null

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/deserializer/EmailDeserializer.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/deserializer/EmailDeserializer.java
@@ -3,6 +3,7 @@ package fr.dossierfacile.common.deserializer;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.Locale;
@@ -19,6 +20,6 @@ public class EmailDeserializer extends StdDeserializer<String> {
 
     @Override
     public String deserialize(JsonParser jsonparser, DeserializationContext context) throws IOException {
-        return (jsonparser.getText() == null) ? null : jsonparser.getText().toLowerCase(Locale.ROOT);
+        return (StringUtils.isEmpty(jsonparser.getText())) ? null : jsonparser.getText().toLowerCase(Locale.ROOT);
     }
 }
