560cfe4c324657d63e197eb835a53d5a78664459
Matthieu Bollot <m.bollot@code-troopers.com>
2023-01-02 17:48:28 +0100

    feat(GC): 🎸 Avoid to remove preview in GC

--- dossierfacile-garbage-collector/src/main/java/fr/dossierfacile/garbagecollector/repo/file/FileRepository.java
+++ dossierfacile-garbage-collector/src/main/java/fr/dossierfacile/garbagecollector/repo/file/FileRepository.java
@@ -9,7 +9,7 @@ public interface FileRepository extends JpaRepository<File, Long> {
 
     @Query(value = "select\n" +
             "EXISTS (select d from Document d where d.name=:path) or \n" +
-            "EXISTS (select f from File f where f.path=:path) or \n" +
+            "EXISTS (select f from File f where f.path=:path or f.preview=:path) or \n" +
             "EXISTS (select apt from apartment_sharing apt where apt.url_dossier_pdf_document=:path)", nativeQuery = true)
     boolean existsObject(@Param("path") String path);
 }
