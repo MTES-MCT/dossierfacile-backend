c9efd72fc5403c2f9ad16986f39b0130dbcf23c1
Matthieu Bollot <m.bollot@code-troopers.com>
2023-01-06 16:23:54 +0100

    fix(tenant): 🐛 Refactor and fix send of weekly warnings

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/TenantServiceImpl.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/TenantServiceImpl.java
@@ -1,5 +1,6 @@
 package fr.dossierfacile.api.front.service;
 
+import fr.dossierfacile.api.front.exception.ConfirmationTokenNotFoundException;
 import fr.dossierfacile.api.front.form.SubscriptionApartmentSharingOfTenantForm;
 import fr.dossierfacile.api.front.model.tenant.EmailExistsModel;
 import fr.dossierfacile.api.front.model.tenant.TenantModel;
@@ -7,21 +8,34 @@ import fr.dossierfacile.api.front.register.RegisterFactory;
 import fr.dossierfacile.api.front.register.enums.StepRegister;
 import fr.dossierfacile.api.front.register.form.partner.EmailExistsForm;
 import fr.dossierfacile.api.front.repository.ApartmentSharingRepository;
+import fr.dossierfacile.api.front.repository.ConfirmationTokenRepository;
 import fr.dossierfacile.api.front.repository.PropertyApartmentSharingRepository;
+import fr.dossierfacile.api.front.service.interfaces.ConfirmationTokenService;
+import fr.dossierfacile.api.front.service.interfaces.DocumentService;
+import fr.dossierfacile.api.front.service.interfaces.GuarantorService;
+import fr.dossierfacile.api.front.service.interfaces.LogService;
+import fr.dossierfacile.api.front.service.interfaces.MailService;
 import fr.dossierfacile.api.front.service.interfaces.PropertyService;
 import fr.dossierfacile.api.front.service.interfaces.TenantService;
 import fr.dossierfacile.api.front.util.Obfuscator;
 import fr.dossierfacile.common.entity.ApartmentSharing;
+import fr.dossierfacile.common.entity.ConfirmationToken;
 import fr.dossierfacile.common.entity.Property;
 import fr.dossierfacile.common.entity.PropertyApartmentSharing;
 import fr.dossierfacile.common.entity.Tenant;
+import fr.dossierfacile.common.enums.LogType;
 import fr.dossierfacile.common.enums.PartnerCallBackType;
 import fr.dossierfacile.common.enums.TenantFileStatus;
 import fr.dossierfacile.common.enums.TenantType;
 import fr.dossierfacile.common.repository.TenantCommonRepository;
 import fr.dossierfacile.common.service.interfaces.PartnerCallBackService;
+import io.sentry.Sentry;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,12 +45,18 @@ import java.time.LocalDateTime;
 @Service
 @AllArgsConstructor
 public class TenantServiceImpl implements TenantService {
-    private final RegisterFactory registerFactory;
-    private final PropertyService propertyService;
-    private final PropertyApartmentSharingRepository propertyApartmentSharingRepository;
-    private final TenantCommonRepository tenantRepository;
-    private final PartnerCallBackService partnerCallBackService;
     private final ApartmentSharingRepository apartmentSharingRepository;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+    private final DocumentService documentService;
+    private final LogService logService;
+    private final MailService mailService;
+    private final GuarantorService guarantorService;
+    private final PropertyApartmentSharingRepository propertyApartmentSharingRepository;
+    private final PartnerCallBackService partnerCallBackService;
+    private final PropertyService propertyService;
+    private final RegisterFactory registerFactory;
+    private final TenantCommonRepository tenantRepository;
 
     @Override
     public <T> TenantModel saveStepRegister(Tenant tenant, T formStep, StepRegister step) {
@@ -109,13 +129,73 @@ public class TenantServiceImpl implements TenantService {
                 .exists(tenantRepository.existsByEmail(emailExistsForm.getEmail()))
                 .build();
     }
+
     @Override
     public Tenant findById(Long id) {
         return tenantRepository.findById(id).get();
     }
+
     @Override
-    public Tenant findByKeycloakId(String keycloakId){
+    public Tenant findByKeycloakId(String keycloakId) {
         return tenantRepository.findByKeycloakId(keycloakId);
     }
 
+    @Transactional
+    @Override
+    public void processWarningsBatch(LocalDateTime localDateTime, int warnings, Pageable page) {
+        Page<Tenant> tenantList = tenantRepository.findByLastLoginDateIsBeforeAndHasDocuments(page, localDateTime, warnings);
+
+        switch (warnings) {
+            case 0 ->
+                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants who will be warned for FIRST time by email");
+            case 1 ->
+                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants who will be warned for SECOND time by email");
+            case 2 ->
+                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants whose documents will be deleted");
+        }
+
+        tenantList.stream().parallel().forEach(t -> {
+            try {
+                handleTenantWarning(t, warnings);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                Sentry.captureException(e);
+            }
+        });
+    }
+
+    private void handleTenantWarning(Tenant t, int warnings) {
+        switch (warnings) {
+            case 0 -> {
+                log.info("accountWarnings. FIRST warning for tenant with ID [" + t.getId() + "]");
+                t.setWarnings(1);
+                mailService.sendEmailFirstWarningForDeletionOfDocuments(t, confirmationTokenService.createToken(t));
+                logService.saveLog(LogType.FIRST_ACCOUNT_WARNING_FOR_DOCUMENT_DELETION, t.getId());
+            }
+            case 1 -> {
+                log.info("accountWarnings. SECOND warning for tenant with ID [" + t.getId() + "]");
+                t.setWarnings(2);
+                mailService.sendEmailSecondWarningForDeletionOfDocuments(t, confirmationTokenService.createToken(t));
+                logService.saveLog(LogType.SECOND_ACCOUNT_WARNING_FOR_DOCUMENT_DELETION, t.getId());
+            }
+            case 2 -> {
+                log.info("accountWarnings. Documents deletion for tenant with ID [" + t.getId() + "]");
+                t.setWarnings(0);
+                t.setConfirmationToken(null);
+                t.setHonorDeclaration(false);
+                t.setStatus(TenantFileStatus.ARCHIVED);
+                t.setZipCode("");
+                t.setClarification("");
+
+                ConfirmationToken confirmationToken = confirmationTokenRepository.findByUser(t).orElseThrow(() -> new ConfirmationTokenNotFoundException(t.getId()));
+                confirmationTokenRepository.delete(confirmationToken);
+
+                documentService.deleteAllDocumentsAssociatedToTenant(t);
+                guarantorService.deleteAllGuaratorsAssociatedToTenant(t);
+                logService.saveLog(LogType.DOCUMENT_DELETION_AFTER_2_ACCOUNT_WARNINGS, t.getId());
+                partnerCallBackService.sendCallBack(t, PartnerCallBackType.ARCHIVED_ACCOUNT);
+            }
+        }
+        tenantRepository.save(t);
+    }
 }
