a365e9c0775ae9af11991e92f1fa217096273dfe
Juliette <juliette@dossierfacile.fr>
2023-01-18 12:04:43 +0100

    Add partner endpoint

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/partner/controller/ApiPartnerMessageController.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/partner/controller/ApiPartnerMessageController.java
@@ -4,6 +4,7 @@ import fr.dossierfacile.api.front.form.MessageForm;
 import fr.dossierfacile.api.front.model.MessageModel;
 import fr.dossierfacile.api.front.security.interfaces.AuthenticationFacade;
 import fr.dossierfacile.api.front.service.interfaces.MessageService;
+import fr.dossierfacile.common.entity.Tenant;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +29,13 @@ public class ApiPartnerMessageController {
     @GetMapping("")
     public ResponseEntity<List<MessageModel>> all(@PathVariable Long tenantId) {
         return ResponseEntity.ok(messageService.findAll(authenticationFacade.getTenant(tenantId)));
+    }
+
+    @PutMapping("/read")
+    public ResponseEntity<List<MessageModel>> markAsRead(@PathVariable Long tenantId) {
+        Tenant tenant = authenticationFacade.getTenant(tenantId);
+        List<MessageModel> messages = messageService.markMessagesAsRead(tenant);
+        return ResponseEntity.ok(messages);
     }
 
     @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
