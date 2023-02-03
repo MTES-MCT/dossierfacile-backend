9edc0d9583fcd4945bbe0041c82f9b6872852ce8
Fabien <fabien@dossierfacile.fr>
2023-02-03 15:08:19 +0100

    fix: enable asynchronous call - remove if on critical path

---
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/AsyncConfig.java
@@ -0,0 +1,19 @@
+package fr.dossierfacile.api.front.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync(proxyTargetClass = true)
+public class AsyncConfig implements AsyncConfigurer {
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.initialize();
+        return threadPoolTaskExecutor;
+    }
+}
