fc62dd26eb3161f03c0a8e9714cf2f24ed0e417d
Juliette <juliette@dossierfacile.fr>
2023-02-15 14:51:27 +0100

    refactor(process-file): Encapsulate QrCode and add it to ValidationResult when processing MFC files

---
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/util/QrCode.java
@@ -0,0 +1,12 @@
+package fr.dossierfacile.process.file.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class QrCode {
+
+    private final String content;
+
+}
