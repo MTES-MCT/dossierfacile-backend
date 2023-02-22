447929513e542d0f896b073bd1b174a8cc8abc57
Fabien <fabien@dossierfacile.fr>
2023-02-22 13:16:48 +0100

    feat: add partner application endpoint

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/UserApiServiceImpl.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/UserApiServiceImpl.java
@@ -3,19 +3,36 @@ package fr.dossierfacile.api.front.service;
 import fr.dossierfacile.api.front.exception.UserApiNotFoundException;
 import fr.dossierfacile.api.front.repository.UserApiRepository;
 import fr.dossierfacile.api.front.service.interfaces.UserApiService;
+import fr.dossierfacile.common.entity.Tenant;
+import fr.dossierfacile.common.entity.TenantUserApi;
 import fr.dossierfacile.common.entity.UserApi;
+import fr.dossierfacile.common.repository.TenantUserApiRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserApiServiceImpl implements UserApiService {
 
     private final UserApiRepository userApiRepository;
+    private final TenantUserApiRepository tenantUserApiRepository;
 
     @Override
     public UserApi findById(Long id) {
         return userApiRepository.findById(id)
                 .orElseThrow(() -> new UserApiNotFoundException(id));
     }
+    @Override
+    public Optional<UserApi> findByName(String partner) {
+        return userApiRepository.findByName(partner);
+    }
+    @Override
+    public boolean anyTenantIsAssociated(UserApi partner, List<Tenant> tenants) {
+        Optional<TenantUserApi> result = tenantUserApiRepository.findFirstByUserApiAndTenantIn(partner, tenants);
+        return result.isPresent();
+    }
+
 }
--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/repository/TenantUserApiRepository.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/repository/TenantUserApiRepository.java
@@ -6,10 +6,13 @@ import fr.dossierfacile.common.entity.TenantUserApiKey;
 import fr.dossierfacile.common.entity.UserApi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TenantUserApiRepository extends JpaRepository<TenantUserApi, TenantUserApiKey> {
     Optional<TenantUserApi> findFirstByTenantAndUserApi(Tenant tenant, UserApi userApi);
 
     Optional<TenantUserApi> findFirstByTenantIdAndUserApiName(Long tenantId, String userApiName);
+
+    Optional<TenantUserApi> findFirstByUserApiAndTenantIn(UserApi partner, List<Tenant> tenants);
 }
