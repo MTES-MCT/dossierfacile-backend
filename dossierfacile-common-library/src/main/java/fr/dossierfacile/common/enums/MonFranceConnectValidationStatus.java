15e5e9fa9ebf03cbc0b2527490be7efcd5cefc7f
Juliette <juliette@dossierfacile.fr>
2023-02-23 18:02:55 +0100

    feat(process-file): Add error status if MFC doesn't find the requested document

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/enums/MonFranceConnectValidationStatus.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/enums/MonFranceConnectValidationStatus.java
@@ -4,7 +4,8 @@ public enum MonFranceConnectValidationStatus {
 
     VALID,
     INVALID,
-    API_ERROR;
+    API_ERROR,
+    UNKNOWN_DOCUMENT;
 
     public static MonFranceConnectValidationStatus of(boolean isValid) {
         return isValid ? VALID : INVALID;
