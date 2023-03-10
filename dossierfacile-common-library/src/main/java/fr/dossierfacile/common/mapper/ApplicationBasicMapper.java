7328b590846c2d2197cedaa8817abd877e9e91c3
Fabien <fabien@dossierfacile.fr>
2023-03-10 13:56:36 +0100

    feat: add format parameter to apartmentSharing listing

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/mapper/ApplicationBasicMapper.java
@@ -0,0 +1,23 @@
+package fr.dossierfacile.common.mapper;
+
+import fr.dossierfacile.common.entity.ApartmentSharing;
+import fr.dossierfacile.common.entity.Tenant;
+import fr.dossierfacile.common.model.apartment_sharing.ApplicationModel;
+import fr.dossierfacile.common.model.apartment_sharing.TenantModel;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
+
+@Component
+@Mapper(componentModel = "spring")
+public interface ApplicationBasicMapper extends ApartmentSharingMapper {
+
+    ApplicationModel toApplicationModel(ApartmentSharing apartmentSharing);
+
+    @Mapping(target = "allowCheckTax", ignore = true)
+    @Mapping(target = "franceConnect", ignore = true)
+    @Mapping(target = "guarantors", ignore = true)
+    @Mapping(target = "documents", ignore = true)
+    TenantModel toTenantModel(Tenant tenant);
+
+}
