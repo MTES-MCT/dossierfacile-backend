250a567a978dfa0f82d52ba3035758ae0eca4889
Juliette <juliette@dossierfacile.fr>
2023-03-16 09:58:40 +0100

    feat(process-file): Display document issuer and category check in BO

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/entity/DocumentIssuer.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/entity/DocumentIssuer.java
@@ -1,8 +1,15 @@
 package fr.dossierfacile.common.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum DocumentIssuer {
 
-    MON_FRANCE_CONNECT,
-    UNKNOWN
+    MON_FRANCE_CONNECT("Mon FranceConnect"),
+    UNKNOWN("Émetteur inconnu");
+
+    private final String label;
 
 }
