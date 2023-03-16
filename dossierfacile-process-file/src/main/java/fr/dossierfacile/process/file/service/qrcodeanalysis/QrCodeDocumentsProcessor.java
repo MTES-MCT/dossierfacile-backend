a68bcdfb060af46abadc98013ca349281f60dca9
Juliette <juliette@dossierfacile.fr>
2023-03-16 11:44:49 +0100

    refactor(process-file): Introduce QrCodeDocumentIssuer to allow multiple issuers

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/qrcodeanalysis/QrCodeDocumentsProcessor.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/qrcodeanalysis/QrCodeDocumentsProcessor.java
@@ -6,8 +6,6 @@ import fr.dossierfacile.common.entity.QrCodeFileAnalysis;
 import fr.dossierfacile.common.enums.DocumentCategory;
 import fr.dossierfacile.common.service.interfaces.FileStorageService;
 import fr.dossierfacile.process.file.repository.QrCodeFileAnalysisRepository;
-import fr.dossierfacile.process.file.service.qrcodeanalysis.monfranceconnect.FileAuthenticator;
-import fr.dossierfacile.process.file.service.qrcodeanalysis.monfranceconnect.MonFranceConnectDocumentType;
 import fr.dossierfacile.process.file.util.Documents;
 import fr.dossierfacile.process.file.util.InMemoryPdfFile;
 import fr.dossierfacile.process.file.util.QrCode;
@@ -32,7 +30,8 @@ public class QrCodeDocumentsProcessor {
     private static final List<DocumentCategory> CATEGORIES_TO_PROCESS = List.of(TAX, FINANCIAL, PROFESSIONAL);
     private final String PDF_TYPE = "application/pdf";
 
-    private final FileAuthenticator fileAuthenticator;
+    private final List<QrCodeDocumentIssuer<? extends AuthenticationRequest>> issuers;
+
     private final QrCodeFileAnalysisRepository analysisRepository;
     private final FileStorageService fileStorageService;
 
@@ -43,13 +42,14 @@ public class QrCodeDocumentsProcessor {
             document.getFiles().stream()
                     .filter(analysisRepository::hasNotAlreadyBeenAnalyzed)
                     .filter(file -> PDF_TYPE.equals(file.getContentType()))
-                    .forEach(file -> downloadAndAnalyze(file, document).ifPresent(analysisRepository::save));
+                    .forEach(file -> downloadAndAnalyze(file, document)
+                            .ifPresent(analysis -> save(file, analysis)));
         }
     }
 
     private Optional<QrCodeFileAnalysis> downloadAndAnalyze(File file, Document document) {
         try (InMemoryPdfFile inMemoryPdfFile = InMemoryPdfFile.create(file, fileStorageService)) {
-            return analyze(file, document, inMemoryPdfFile);
+            return analyze(document, inMemoryPdfFile);
         } catch (IOException e) {
             log.error("Unable to download file " + file.getPath(), e);
             Sentry.captureMessage("Unable to download file " + file.getPath());
@@ -57,18 +57,38 @@ public class QrCodeDocumentsProcessor {
         return Optional.empty();
     }
 
-    private Optional<QrCodeFileAnalysis> analyze(File file, Document document, InMemoryPdfFile inMemoryPdfFile) {
-        if (!inMemoryPdfFile.hasQrCode()) {
+    private Optional<QrCodeFileAnalysis> analyze(Document document, InMemoryPdfFile file) {
+        if (!file.hasQrCode()) {
             return Optional.empty();
         }
-        boolean isAllowedInCurrentCategory = MonFranceConnectDocumentType.of(inMemoryPdfFile)
-                .getCategory()
-                .map(guess -> guess.isMatchingCategoryOf(document))
-                .orElse(true);
 
-        QrCode qrCode = inMemoryPdfFile.getQrCode();
-        return fileAuthenticator.authenticate(inMemoryPdfFile, qrCode)
-                .map(result -> result.toAnalysisResult(file, qrCode, isAllowedInCurrentCategory));
+        for (QrCodeDocumentIssuer<?> issuer : issuers) {
+            Optional<AuthenticationResult> result = issuer.tryToAuthenticate(file);
+            if (result.isPresent()) {
+                boolean allowedInCurrentCategory = issuer.guessCategory(file)
+                        .map(guess -> guess.isMatchingCategoryOf(document))
+                        .orElse(true);
+                QrCodeFileAnalysis fileAnalysis = buildAnalysis(result.get(), file.getQrCode(), allowedInCurrentCategory);
+                return Optional.of(fileAnalysis);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private void save(File file, QrCodeFileAnalysis analysis) {
+        analysis.setFile(file);
+        analysisRepository.save(analysis);
+    }
+
+    public QrCodeFileAnalysis buildAnalysis(AuthenticationResult result, QrCode qrCode, boolean isAllowedInDocumentCategory) {
+        QrCodeFileAnalysis analysis = new QrCodeFileAnalysis();
+        analysis.setIssuerName(result.getIssuerName());
+        analysis.setQrCodeContent(qrCode.getContent());
+        analysis.setApiResponse(result.getApiResponse());
+        analysis.setAuthenticationStatus(result.getAuthenticationStatus());
+        analysis.setAllowedInDocumentCategory(isAllowedInDocumentCategory);
+        return analysis;
     }
 
 }
