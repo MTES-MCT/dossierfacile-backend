8a202e93edb8b030826aa92953b61dbb70a70034
Juliette <juliette@dossierfacile.fr>
2023-03-22 11:23:36 +0100

    feat(bo): Add times a tenant has appeared in BO since last validation

--- dossierfacile-bo/src/main/java/fr/gouv/bo/dto/TenantInfoHeader.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/dto/TenantInfoHeader.java
@@ -1,24 +1,34 @@
 package fr.gouv.bo.dto;
 
+import fr.dossierfacile.common.entity.Log;
 import fr.dossierfacile.common.entity.Tenant;
 import fr.dossierfacile.common.entity.UserApi;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.ToString;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static fr.dossierfacile.common.enums.LogType.ACCOUNT_DENIED;
+import static fr.dossierfacile.common.enums.LogType.ACCOUNT_VALIDATED;
+import static lombok.AccessLevel.PRIVATE;
 
 @Getter
-@AllArgsConstructor
+@AllArgsConstructor(access = PRIVATE)
 public class TenantInfoHeader {
 
     private final List<HeaderElement> elements;
 
-    public static TenantInfoHeader build(Tenant tenant, List<UserApi> partners) {
+    public static TenantInfoHeader build(Tenant tenant, List<UserApi> partners, List<Log> tenantLogs) {
         List<HeaderElement> elements = List.of(
                 new HeaderElement("Nom", tenant.getFullName()),
                 new HeaderElement("Dossier", getApplicationType(tenant)),
-                new HeaderElement("Partenaires", getPartnerNames(partners))
+                new HeaderElement("Partenaires", getPartnerNames(partners)),
+                new HeaderElement("Passages en BO", getTimesTenantAppearedInBoSinceLastValidation(tenantLogs))
         );
         return new TenantInfoHeader(elements);
     }
@@ -37,9 +47,19 @@ public class TenantInfoHeader {
         };
     }
 
+    private static String getTimesTenantAppearedInBoSinceLastValidation(List<Log> tenantLogs) {
+        Stream<Log> deniedSinceLastValidation = tenantLogs.stream()
+                .filter(log -> List.of(ACCOUNT_DENIED, ACCOUNT_VALIDATED).contains(log.getLogType()))
+                .sorted(Comparator.comparing(Log::getCreationDateTime).reversed())
+                .takeWhile(log -> log.getLogType() == ACCOUNT_DENIED);
+        return String.valueOf(deniedSinceLastValidation.count() + 1);
+    }
+
     @Getter
+    @ToString
+    @EqualsAndHashCode
     @AllArgsConstructor
-    private static class HeaderElement {
+    static class HeaderElement {
         private final String label;
         private final String value;
     }
