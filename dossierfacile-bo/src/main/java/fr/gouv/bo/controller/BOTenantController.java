dc271cece10ddd886ec9816891f4fc279635ad87
Matthieu Bollot <m.bollot@code-troopers.com>
2023-05-03 15:29:01 +0200

    feat(bo): Add AvisSituation detected data

--- dossierfacile-bo/src/main/java/fr/gouv/bo/controller/BOTenantController.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/controller/BOTenantController.java
@@ -283,6 +283,7 @@ public class BOTenantController {
                 if (document.getDocumentStatus().equals(DocumentStatus.TO_PROCESS)) {
                     guarantorItem.getMessageItems().add(MessageItem.builder()
                             .monthlySum(document.getMonthlySum())
+                            .avisDetected(document.getAvisDetected())
                             .customTex(document.getCustomText())
                             .taxDocument(document.getTaxProcessResult())
                             .documentCategory(document.getDocumentCategory())
