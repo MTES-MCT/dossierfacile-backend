b17fe6a50771ff01a0c1b45b98adc05d698f81c5
Matthieu Bollot <m.bollot@code-troopers.com>
2023-05-23 16:09:36 +0200

    feat(owner): Add token for callback

---
+++ dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/config/WebhookFilter.java
@@ -0,0 +1,58 @@
+package fr.dossierfacile.api.dossierfacileapiowner.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.NegatedRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class WebhookFilter extends OncePerRequestFilter {
+    private final String token;
+    private final String headerName;
+
+    private final RequestMatcher uriMatcher =
+            new AntPathRequestMatcher("/webhook/**", HttpMethod.POST.name());
+
+    public WebhookFilter(String token, String headerName) {
+        this.token = token;
+        this.headerName = headerName;
+    }
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws IOException, ServletException {
+        String apiKey = getApiKey(request);
+        if(apiKey != null) {
+            if(!apiKey.equals(token)) {
+                response.setStatus(401);
+                response.getWriter().write("Invalid API Key");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+
+    private String getApiKey(HttpServletRequest httpRequest) {
+        String apiKey = httpRequest.getHeader(headerName);
+        if(apiKey != null) {
+            apiKey = apiKey.trim();
+        }
+        return apiKey;
+    }
+
+    @Override
+    protected boolean shouldNotFilter(@NotNull HttpServletRequest request) {
+        RequestMatcher matcher = new NegatedRequestMatcher(uriMatcher);
+        return matcher.matches(request);
+    }
+
+
+}
