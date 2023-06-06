6ebcc7548ae92c17197401e598a3e4963fef5c2d
Matthieu Bollot <m.bollot@code-troopers.com>
2023-06-06 11:50:12 +0200

    Revert "feat(tenant): Move webhook after delete (#448)" (#450)
    This reverts commit f24f0385cc8521d12d545cfbca96e52f1638d859.

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/UserServiceImpl.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/UserServiceImpl.java
@@ -140,6 +140,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void deleteAccount(Tenant tenant) {
+        partnerCallBackService.sendCallBack(tenant, PartnerCallBackType.DELETED_ACCOUNT);
         saveAndDeleteInfoByTenant(tenant);
         logService.saveLog(LogType.ACCOUNT_DELETE, tenant.getId());
         if (tenant.getTenantType() == TenantType.CREATE) {
@@ -150,7 +151,6 @@ public class UserServiceImpl implements UserService {
             userRepository.delete(tenant);
             apartmentSharingService.removeTenant(tenant.getApartmentSharing(), tenant);
         }
-        partnerCallBackService.sendCallBack(tenant, PartnerCallBackType.DELETED_ACCOUNT);
     }
 
     @Override
