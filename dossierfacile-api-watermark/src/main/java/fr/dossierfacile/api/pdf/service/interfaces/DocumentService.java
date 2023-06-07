10552166686f68b2cd654a3bd7ea3a8fdce6536a
Fabien <fabien@dossierfacile.fr>
2023-06-07 14:08:06 +0200

    feat(watermark): refactor using storage file

--- dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/service/interfaces/DocumentService.java
+++ dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/service/interfaces/DocumentService.java
@@ -3,8 +3,9 @@ package fr.dossierfacile.api.pdf.service.interfaces;
 import fr.dossierfacile.api.pdf.form.DocumentForm;
 import fr.dossierfacile.api.pdf.response.DocumentUrlResponse;
 import fr.dossierfacile.api.pdf.response.UploadFilesResponse;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
+
+import javax.servlet.http.HttpServletResponse;
 
 public interface DocumentService {
 
---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/entity/WatermarkDocument.java
@@ -0,0 +1,69 @@
+package fr.dossierfacile.common.entity;
+
+import fr.dossierfacile.common.enums.FileStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity(name = "watermark_document")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class WatermarkDocument {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Nullable
+    @CreatedDate
+    private Date createdDate;
+
+    private String token;
+
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @JoinTable(
+            name = "watermark_document_storage_file",
+            joinColumns = @JoinColumn(name = "watermark_document_id"),
+            inverseJoinColumns = @JoinColumn(name = "storage_file_id")
+    )
+    private List<StorageFile> files = new ArrayList<>();
+
+    @Enumerated(EnumType.STRING)
+    private FileStatus pdfStatus;
+
+    @OneToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "pdf_file_id")
+    private StorageFile pdfFile;
+
+    private String text;
+}
