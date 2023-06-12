c97916b6ac04aaf454c904cf0edd7b72293a0fdb
Fabien <fabien@dossierfacile.fr>
2023-06-12 18:13:02 +0200

    feat(bo): manufacture - split zip for avoiding OOM

--- dossierfacile-bo/src/main/java/fr/gouv/bo/service/ToolsService.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/service/ToolsService.java
@@ -48,47 +48,30 @@ public class ToolsService {
 
     public void generateLastTreatedDossiers(int count) throws Exception {
         // First generate the "count" dossier
-        Pageable pageable = PageRequest.of(
-                0,
-                count,
-                Sort.by(Sort.Direction.DESC, "lastUpdateDate")
-        );
-        List<Tenant> tenants = tenantCommonRepository.findTenantsToExtract(pageable);
+        for ( int i = 0 ; i < (count/10) ; i++) {
+            Pageable pageable = PageRequest.of(
+                    i,
+                    10,
+                    Sort.by(Sort.Direction.DESC, "lastUpdateDate")
+            );
+            List<Tenant> tenants = tenantCommonRepository.findTenantsToExtract(pageable);
 
-        // then create the zip
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        String zipName = "zip-" + UUID.randomUUID();
-        // Création d'un flux de sortie vers le fichier ZIP
-        FileOutputStream fos = new FileOutputStream(zipName);
-        ZipOutputStream zos = new ZipOutputStream(fos);
+            // then create the zip
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.registerModule(new JavaTimeModule());
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            String zipName = "zip-" + UUID.randomUUID();
+            // Création d'un flux de sortie vers le fichier ZIP
+            FileOutputStream fos = new FileOutputStream(zipName);
+            ZipOutputStream zos = new ZipOutputStream(fos);
 
-        for (Tenant tenant : tenants) {
+            for (Tenant tenant : tenants) {
 
-            TenantModel t = tenantMapper.toTenantModel(tenant);
-            JsonElement je = JsonParser.parseString(objectMapper.writeValueAsString(t));
+                TenantModel t = tenantMapper.toTenantModel(tenant);
+                JsonElement je = JsonParser.parseString(objectMapper.writeValueAsString(t));
 
-            // on traite les fichiers
-            for (Document d : tenant.getDocuments()) {
-                try (InputStream fileInputStream = fileStorageService.download(d.getWatermarkFile())) {
-
-                    zos.putNextEntry(new ZipEntry("tid-" + t.getId() + "/" + d.getWatermarkFile().getPath() + ".pdf"));
-
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = fileInputStream.read(buffer)) > 0) {
-                        zos.write(buffer, 0, length);
-                    }
-                    zos.closeEntry();
-                } catch (Exception e) {
-                    log.error("Error manufacture", e);
-                    zos.closeEntry();
-                }
-            }
-            // guarantor
-            for (Guarantor g : tenant.getGuarantors()) {
-                for (Document d : g.getDocuments()) {
+                // on traite les fichiers
+                for (Document d : tenant.getDocuments()) {
                     try (InputStream fileInputStream = fileStorageService.download(d.getWatermarkFile())) {
 
                         zos.putNextEntry(new ZipEntry("tid-" + t.getId() + "/" + d.getWatermarkFile().getPath() + ".pdf"));
@@ -104,44 +87,62 @@ public class ToolsService {
                         zos.closeEntry();
                     }
                 }
-            }
-            // on traite le tenant en json
-            String tenantString = gson.toJson(je);
-            byte[] bytes = tenantString.getBytes();
-            InputStream inputStream = new ByteArrayInputStream(bytes);
+                // guarantor
+                for (Guarantor g : tenant.getGuarantors()) {
+                    for (Document d : g.getDocuments()) {
+                        try (InputStream fileInputStream = fileStorageService.download(d.getWatermarkFile())) {
 
-            try {
-                zos.putNextEntry(new ZipEntry("tid-" + t.getId() + ".json"));
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = inputStream.read(buffer)) > 0) {
-                    zos.write(buffer, 0, length);
+                            zos.putNextEntry(new ZipEntry("tid-" + t.getId() + "/" + d.getWatermarkFile().getPath() + ".pdf"));
+
+                            byte[] buffer = new byte[1024];
+                            int length;
+                            while ((length = fileInputStream.read(buffer)) > 0) {
+                                zos.write(buffer, 0, length);
+                            }
+                            zos.closeEntry();
+                        } catch (Exception e) {
+                            log.error("Error manufacture", e);
+                            zos.closeEntry();
+                        }
+                    }
                 }
-                zos.closeEntry();
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.error("Error manufacture", e);
-                zos.closeEntry();
+                // on traite le tenant en json
+                String tenantString = gson.toJson(je);
+                byte[] bytes = tenantString.getBytes();
+                InputStream inputStream = new ByteArrayInputStream(bytes);
+
+                try {
+                    zos.putNextEntry(new ZipEntry("tid-" + t.getId() + ".json"));
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+                    zos.closeEntry();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    log.error("Error manufacture", e);
+                    zos.closeEntry();
+                }
+            }
+
+            zos.close();
+
+            //then push the zip
+            try {
+                File file = new File(zipName);
+
+                try (InputStream zipInputStream = new FileInputStream(file)) {
+                    StorageFile sfile = StorageFile.builder()
+                            .name("zip-manufacture.zip")
+                            .contentType("application/zip")
+                            .build();
+                    fileStorageService.upload(zipInputStream, sfile);
+                }
+            } catch (IOException e) {
+                log.error("zip probleme");
             }
         }
-
-        zos.close();
-
-        //then push the zip
-        try {
-            File file = new File(zipName);
-
-            try (InputStream zipInputStream = new FileInputStream(file)) {
-                StorageFile sfile = StorageFile.builder()
-                        .name("zip-manufacture.zip")
-                        .contentType("application/zip")
-                        .build();
-                fileStorageService.upload(zipInputStream, sfile);
-            }
-        } catch (IOException e) {
-            log.error("zip probleme");
-        }
-
 
     }
 
