8fb830fffe6f112173431e858e460608414ecbde
Juliette <juliette@dossierfacile.fr>
2023-06-28 16:14:51 +0200

    feat(process-file): Read 2D-Doc on SNCF payslip

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/TwoDDocReader.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/TwoDDocReader.java
@@ -1,16 +1,14 @@
 package fr.dossierfacile.process.file.barcode.twoddoc;
 
-import com.google.zxing.BarcodeFormat;
 import com.google.zxing.BinaryBitmap;
-import com.google.zxing.DecodeHintType;
-import com.google.zxing.MultiFormatReader;
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.Result;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.common.HybridBinarizer;
-import com.google.zxing.multi.GenericMultipleBarcodeReader;
-import com.google.zxing.multi.MultipleBarcodeReader;
-import io.sentry.Sentry;
+import com.google.zxing.datamatrix.DataMatrixReader;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
@@ -18,57 +16,69 @@ import org.apache.pdfbox.rendering.PDFRenderer;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-import java.util.EnumMap;
-import java.util.EnumSet;
-import java.util.Map;
 import java.util.Optional;
 
 @Slf4j
 public class TwoDDocReader {
 
-	private static final Map<DecodeHintType, Object> HINTS;
-
-    static {
-        HINTS = new EnumMap<>(DecodeHintType.class);
-        HINTS.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
-        HINTS.put(DecodeHintType.POSSIBLE_FORMATS, EnumSet.allOf(BarcodeFormat.class));
-    }
+    private static final DataMatrixReader READER = new DataMatrixReader();
 
     public static Optional<TwoDDocRawContent> find2DDocOn(PDDocument document) {
         if (document.isEncrypted()) {
             return Optional.empty();
         }
 
+        for (PossibleCodePosition possibleCodePosition : PossibleCodePosition.values()) {
+            Optional<Result> result = tryReadingCode(document, possibleCodePosition);
+            if (result.isPresent()) {
+                return result.map(Result::getText).map(TwoDDocRawContent::new);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    private static Optional<Result> tryReadingCode(PDDocument document, PossibleCodePosition possibleCodePosition) {
         try {
-            BinaryBitmap binaryBitmap = buildBinaryBitmap(document);
-            MultipleBarcodeReader multiReader = new GenericMultipleBarcodeReader(new MultiFormatReader());
-            Result[] results = multiReader.decodeMultiple(binaryBitmap, HINTS);
-            return Optional.ofNullable(results[0].getText())
-                    .map(TwoDDocRawContent::new);
+            BinaryBitmap binaryBitmap = possibleCodePosition.buildBinaryBitmap(document);
+            return Optional.of(READER.decode(binaryBitmap));
         } catch (NotFoundException e) {
             return Optional.empty();
-        } catch (IOException e) {
-            log.error("Exception while trying to extract 2D-Doc from pdf (Sentry ID: {})", Sentry.captureException(e), e);
+        } catch (IOException | ChecksumException | FormatException e) {
+            log.error("Exception while trying to extract 2D-Doc from pdf", e);
             return Optional.empty();
         }
     }
 
-    private static BinaryBitmap buildBinaryBitmap(PDDocument document) throws IOException {
-        int scale = Math.max(1, (int) (2048 / document.getPage(0).getMediaBox().getWidth()));
+    @AllArgsConstructor
+    private enum PossibleCodePosition {
 
-        PDFRenderer pdfRenderer = new PDFRenderer(document);
-        BufferedImage bufferedImage = pdfRenderer.renderImage(0, scale, ImageType.BINARY);
+        TAX_STATEMENT(15, 10),
+        SNCF_PAYSLIP(65, 5);
 
-        // Crop image to have better recognition by the BarcodeReader library
-        int x = 180 * bufferedImage.getWidth() / 1000;
-        int y = 105 * bufferedImage.getWidth() / 1000;
-        int width = 220 * bufferedImage.getWidth() / 1000;
-        BufferedImage cropImg = bufferedImage.getSubimage(x, y, width, width);
+        private final double xPercent;
+        private final double yPercent;
 
-        return new BinaryBitmap(
-                new HybridBinarizer(
-                        new BufferedImageLuminanceSource(cropImg)
-                ));
+        BinaryBitmap buildBinaryBitmap(PDDocument document) throws IOException {
+            int scale = Math.max(1, (int) (2048 / document.getPage(0).getMediaBox().getWidth()));
+
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
+            BufferedImage bufferedImage = pdfRenderer.renderImage(0, scale, ImageType.BINARY);
+
+            return new BinaryBitmap(
+                    new HybridBinarizer(
+                            new BufferedImageLuminanceSource(
+                                    cropImage(bufferedImage)
+                            )));
+        }
+
+        private BufferedImage cropImage(BufferedImage image) {
+            int x = (int) xPercent * image.getWidth() / 100;
+            int y = (int) yPercent * image.getWidth() / 100;
+            int width = 25 * image.getWidth() / 100;
+            return image.getSubimage(x, y, width, width);
+        }
+
     }
 
 }
