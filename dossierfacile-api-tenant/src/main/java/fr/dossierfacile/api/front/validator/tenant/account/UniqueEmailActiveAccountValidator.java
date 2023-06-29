6726f1229184e700026b7d0df0354dc9393bc6c2
Fabien <fabien@dossierfacile.fr>
2023-06-29 14:35:00 +0200

    fix: allow already registered owner to create tenant from api with same email

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/validator/tenant/account/UniqueEmailActiveAccountValidator.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/validator/tenant/account/UniqueEmailActiveAccountValidator.java
@@ -1,7 +1,7 @@
 package fr.dossierfacile.api.front.validator.tenant.account;
 
-import fr.dossierfacile.api.front.repository.UserRepository;
 import fr.dossierfacile.api.front.validator.anotation.tenant.account.UniqueEmailActiveAccount;
+import fr.dossierfacile.common.repository.TenantCommonRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +12,7 @@ import javax.validation.ConstraintValidatorContext;
 @AllArgsConstructor
 public class UniqueEmailActiveAccountValidator implements ConstraintValidator<UniqueEmailActiveAccount, String> {
 
-    private final UserRepository userRepository;
+    private final TenantCommonRepository tenantRepository;
 
     @Override
     public void initialize(UniqueEmailActiveAccount uniqueEmailActiveAccount) {
@@ -21,6 +21,6 @@ public class UniqueEmailActiveAccountValidator implements ConstraintValidator<Un
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        return userRepository.findOneByEmailAndEnabledTrue(email.toLowerCase()) == null && !email.isEmpty();
+        return !tenantRepository.existsByEmail(email.toLowerCase()) && !email.isEmpty();
     }
 }
