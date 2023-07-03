541d2398cb7e650df270597ae2a39df9890a9197
Fabien <fabien@dossierfacile.fr>
2023-07-03 19:10:43 +0200

    feat(bo): add quota for bo user

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/BOUserRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/BOUserRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BOUserRepository extends JpaRepository<BOUser, Long> {
-    Optional<BOUser> findByEmail(String email);;
+    Optional<BOUser> findByEmail(String email);
 
     @Query("SELECT distinct u from User u left join u.userRoles ur where ur.role = 2 or ur.role=3 or ur.role=5 or (u.email like concat('%', lower(:ad)) and u.provider='google')")
     List<BOUser> findAllAdmins(@Param("ad") String ad);
