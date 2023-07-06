9ac229165b197529b6874c0da20409ef69982773
Matthieu Bollot <m.bollot@code-troopers.com>
2023-07-06 15:13:05 +0200

    feat(owner): add log when accessing property

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/repository/PropertyLogRepository.java
@@ -0,0 +1,8 @@
+package fr.dossierfacile.common.repository;
+
+import fr.dossierfacile.common.entity.PropertyLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyLogRepository extends JpaRepository<PropertyLog, Long> {
+
+}
