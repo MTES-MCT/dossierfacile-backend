ab3cf17b8d6f5fa4e7bcb8b80c129079cba3cfd4
Juliette <juliette@dossierfacile.fr>
2023-07-11 14:20:37 +0200

    feat(process-file): Add categories that should have their files analyzed

--- dossierfacile-common-library/src/test/java/fr/dossierfacile/common/FileAnalysisCriteriaTest.java
+++ dossierfacile-common-library/src/test/java/fr/dossierfacile/common/FileAnalysisCriteriaTest.java
@@ -6,9 +6,12 @@ import fr.dossierfacile.common.entity.Tenant;
 import fr.dossierfacile.common.enums.DocumentCategory;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
 import fr.dossierfacile.common.enums.TenantFileStatus;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static fr.dossierfacile.common.enums.DocumentCategory.TAX;
+import static fr.dossierfacile.common.enums.DocumentSubCategory.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FileAnalysisCriteriaTest {
@@ -16,34 +19,34 @@ class FileAnalysisCriteriaTest {
     @ParameterizedTest
     @CsvSource(textBlock = """
             TAX, MY_NAME
+            PROFESSIONAL, CDI
             PROFESSIONAL, STUDENT
             PROFESSIONAL, UNEMPLOYED
             FINANCIAL, SALARY
             FINANCIAL, SCHOLARSHIP
             FINANCIAL, SOCIAL_SERVICE
+            RESIDENCY, GUEST
             """)
     void should_analyze_files_in_categories(DocumentCategory documentCategory, DocumentSubCategory documentSubCategory) {
-        File file = fileInCategory(documentCategory, documentSubCategory);
+        File file = fileInCategory(documentCategory, documentSubCategory, false);
 
         assertThat(FileAnalysisCriteria.shouldBeAnalyzed(file)).isTrue();
     }
 
-    private static File taxFileWithTenantStatus(TenantFileStatus status) {
-        return File.builder()
-                .document(Document.builder()
-                        .tenant(Tenant.builder().status(status).build())
-                        .documentCategory(DocumentCategory.TAX)
-                        .documentSubCategory(DocumentSubCategory.MY_NAME)
-                        .build())
-                .build();
+    @Test
+    void should_not_analyze_document_with_no_files() {
+        File file = fileInCategory(TAX, MY_PARENTS, true);
+
+        assertThat(FileAnalysisCriteria.shouldBeAnalyzed(file)).isFalse();
     }
 
-    private static File fileInCategory(DocumentCategory documentCategory, DocumentSubCategory documentSubCategory) {
+    private static File fileInCategory(DocumentCategory documentCategory, DocumentSubCategory documentSubCategory, boolean noDocument) {
         return File.builder()
                 .document(Document.builder()
                         .tenant(Tenant.builder().status(TenantFileStatus.TO_PROCESS).build())
                         .documentCategory(documentCategory)
                         .documentSubCategory(documentSubCategory)
+                        .noDocument(noDocument)
                         .build())
                 .build();
     }
