ece872476bdc28816a92b326511b12fde268ebdf
Fabien <fabien@dossierfacile.fr>
2023-07-12 17:09:01 +0200

    feat(bo): display error message on error page - filterchain context

--- dossierfacile-bo/src/main/java/fr/gouv/bo/controller/BOErrorController.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/controller/BOErrorController.java
@@ -3,12 +3,20 @@ package fr.gouv.bo.controller;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class BOErrorController implements ErrorController {
+
     @GetMapping(value = "/error", produces = "text/html")
-    public String handleError() {
+    public String handleError(Model model, HttpServletRequest request) {
+        model.addAttribute("statusCode", request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE));
+        model.addAttribute("statusMessage", request.getAttribute(RequestDispatcher.ERROR_MESSAGE));
+        model.addAttribute("errorMessage", request.getAttribute("errorMessage"));
         return "error/error";
     }
 }
