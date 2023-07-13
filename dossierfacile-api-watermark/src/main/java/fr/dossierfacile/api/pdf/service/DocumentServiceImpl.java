4352d2d15af684f50db11d8643650fe37ede6478
Fabien <fabien@dossierfacile.fr>
2023-07-13 16:43:56 +0200

    upgrade libraries

--- dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/service/DocumentServiceImpl.java
+++ dossierfacile-api-watermark/src/main/java/fr/dossierfacile/api/pdf/service/DocumentServiceImpl.java
@@ -16,7 +16,7 @@ import fr.dossierfacile.common.service.interfaces.EncryptionKeyService;
 import fr.dossierfacile.common.service.interfaces.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
