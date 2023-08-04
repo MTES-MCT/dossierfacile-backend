bf669ae4abfbb1790fd61c68257cc4c553caf24a
Fabien <fabien@dossierfacile.fr>
2023-08-04 17:52:15 +0200

    feat: add new owner emails

---
+++ dossierfacile-api-owner/src/main/java/fr/dossierfacile/api/dossierfacileapiowner/tasks/ScheduledTaskService.java
@@ -0,0 +1,36 @@
+package fr.dossierfacile.api.dossierfacileapiowner.tasks;
+
+import fr.dossierfacile.api.dossierfacileapiowner.mail.MailService;
+import fr.dossierfacile.api.dossierfacileapiowner.property.PropertyRepository;
+import fr.dossierfacile.common.entity.Property;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ScheduledTaskService {
+    private final MailService mailService;
+    private final PropertyRepository propertyRepository;
+    @Value("${days_after_validated_property_to_follow_up_email:42}")
+    private Long daysAfterValidatedPropertyToFollowUpEmail;
+
+    @Scheduled(cron = "4 10 0 * * ?")
+    void sendFollowUpValidatedPropertyAfter() {
+        log.info("sendFollowUpValidatedPropertyAfter is launched");
+        LocalDateTime startDate = LocalDateTime.now().minusDays(daysAfterValidatedPropertyToFollowUpEmail).with(LocalTime.MIN);
+        LocalDateTime endDate = LocalDateTime.now().minusDays(daysAfterValidatedPropertyToFollowUpEmail).with(LocalTime.MAX);
+
+        List<Property> properties = propertyRepository.findAllByValidatedIsTrueAndValidatedDateIsBetween(startDate, endDate);
+        for (Property property : properties) {
+            mailService.sendEmailFollowUpValidatedProperty(property.getOwner(), property);
+        }
+    }
+}
