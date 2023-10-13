2cf7a9e0303ca5081f1f90a07f33617bf4f24f30
Fabien <fabien@dossierfacile.fr>
2023-10-13 11:05:22 +0200

    chore: remove account_delete_log

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/AccountDeleteLogRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.gouv.bo.repository;
-
-import fr.dossierfacile.common.entity.AccountDeleteLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountDeleteLogRepository extends JpaRepository<AccountDeleteLog, Long> {
-}
