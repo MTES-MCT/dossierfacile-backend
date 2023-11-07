bce3712984266eab780a6998b2fe4e76ac99ce98
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-07 15:06:21 +0100

    chore(garbage-collector): Remove GUI version of garbage collector

--- dossierfacile-garbage-collector/src/main/java/fr/dossierfacile/garbagecollector/transactions/interfaces/MarkerTransactions.java
+++
@@ -1,9 +0,0 @@
-package fr.dossierfacile.garbagecollector.transactions.interfaces;
-
-import fr.dossierfacile.garbagecollector.model.marker.Marker;
-
-public interface MarkerTransactions {
-    void saveMarkerIfNotYetSaved(String name);
-    void deleteAllMarkers();
-    void deleteMarker(Marker marker);
-}
