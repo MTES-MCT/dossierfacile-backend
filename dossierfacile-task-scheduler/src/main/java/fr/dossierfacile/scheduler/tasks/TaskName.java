92e04e579a3ccbecf14ac239db5475ab2f069001
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-07 18:21:44 +0100

    chore(scheduler): Add task name in logs

---
+++ dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/TaskName.java
@@ -0,0 +1,10 @@
+package fr.dossierfacile.scheduler.tasks;
+
+public enum TaskName {
+
+    GARBAGE_COLLECTION,
+    TENANT_WARNINGS,
+    STORAGE_FILES_BACKUP,
+    STORAGE_FILES_DELETION
+
+}
--- dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/storagesynchronization/DeleteFilesTask.java
+++ dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/storagesynchronization/DeleteFilesTask.java
@@ -3,12 +3,15 @@ package fr.dossierfacile.scheduler.tasks.storagesynchronization;
 import fr.dossierfacile.common.entity.StorageFileToDelete;
 import fr.dossierfacile.common.repository.StorageFileToDeleteRepository;
 import fr.dossierfacile.common.service.interfaces.FileStorageToDeleteService;
+import fr.dossierfacile.scheduler.LoggingContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+import static fr.dossierfacile.scheduler.tasks.TaskName.STORAGE_FILES_DELETION;
 
 @Slf4j
 @Service
@@ -20,7 +23,7 @@ public class DeleteFilesTask {
 
     @Scheduled(fixedDelay = 10000)
     public void deleteFileInProviderTask() {
-        log.info("Starting scheduled deletion of files in object storages");
+        LoggingContext.setTask(STORAGE_FILES_DELETION);
         List<StorageFileToDelete> storageFileToDeleteList = storageFileToDeleteRepository.findAll();
         for (StorageFileToDelete storageFileToDelete : storageFileToDeleteList) {
             fileStorageToDeleteService.delete(storageFileToDelete);
