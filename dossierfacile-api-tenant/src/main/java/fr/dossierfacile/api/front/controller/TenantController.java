99b14d7ee58efdb05854f8602940380f98f8bd7c
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-09 18:52:01 +0100

    feat(tenant): Store acquisition data at account creation

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/controller/TenantController.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/controller/TenantController.java
@@ -1,5 +1,6 @@
 package fr.dossierfacile.api.front.controller;
 
+import fr.dossierfacile.api.front.form.AcquisitionData;
 import fr.dossierfacile.api.front.form.ShareFileByMailForm;
 import fr.dossierfacile.api.front.mapper.PropertyOMapper;
 import fr.dossierfacile.api.front.mapper.TenantMapper;
@@ -17,12 +18,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.badRequest;
@@ -43,8 +46,8 @@ public class TenantController {
     private final UserService userService;
 
     @GetMapping(value = "/profile", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<TenantModel> profile() {
-        Tenant tenant = authenticationFacade.getLoggedTenant();
+    public ResponseEntity<TenantModel> profile(@RequestParam MultiValueMap<String, String> params) {
+        Tenant tenant = authenticationFacade.getLoggedTenant(AcquisitionData.from(params));
         tenantService.updateLastLoginDateAndResetWarnings(tenant);
         return ok(tenantMapper.toTenantModel(tenant));
     }
