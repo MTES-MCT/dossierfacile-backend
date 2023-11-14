d5a6a28277751ae2b403e5633eaf4335d9225ceb
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-11-14 14:58:31 +0100

    feat(task-scheduler): Archive cotenants with archived main tenant

--- dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/tenantwarning/TenantWarningTask.java
+++ dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/tenantwarning/TenantWarningTask.java
@@ -1,9 +1,7 @@
 package fr.dossierfacile.scheduler.tasks.tenantwarning;
 
 import fr.dossierfacile.common.entity.Tenant;
-import fr.dossierfacile.common.repository.TenantCommonRepository;
 import fr.dossierfacile.scheduler.LoggingContext;
-import io.sentry.Sentry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +14,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+import static fr.dossierfacile.scheduler.tasks.TaskName.TENANT_ARCHIVING;
 import static fr.dossierfacile.scheduler.tasks.TaskName.TENANT_WARNINGS;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
@@ -24,57 +23,75 @@ import static org.apache.commons.lang3.StringUtils.isNotBlank;
 @RequiredArgsConstructor
 public class TenantWarningTask {
 
+    private static final int PAGE_SIZE = 100;
+
     @Value("${months_for_deletion_of_documents:3}")
     private Integer monthsForDeletionOfDocuments;
-    private final TenantCommonRepository tenantRepository;
+    private final TenantRepository tenantRepository;
     private final TenantWarningService tenantWarningService;
 
     @Scheduled(cron = "${cron.process.warnings}")
     public void accountWarningsForDocumentDeletion() {
-        LoggingContext.startTask(TENANT_WARNINGS);
         LocalDateTime localDateTime = LocalDateTime.now().minusMonths(monthsForDeletionOfDocuments);
-        processAllWarnings(localDateTime, 2);
-        processAllWarnings(localDateTime, 1);
-        processAllWarnings(localDateTime, 0);
+        archiveAccounts(localDateTime);
+        sendWarningMails(localDateTime);
+    }
+
+    private void archiveAccounts(LocalDateTime limitDate) {
+        LoggingContext.startTask(TENANT_ARCHIVING);
+        processAllWarnings(limitDate, 2);
+        archiveCotenantAccounts();
+        LoggingContext.endTask();
+    }
+
+    private void sendWarningMails(LocalDateTime limitDate) {
+        LoggingContext.startTask(TENANT_WARNINGS);
+        processAllWarnings(limitDate, 1);
+        processAllWarnings(limitDate, 0);
         LoggingContext.endTask();
     }
 
     private void processAllWarnings(LocalDateTime localDateTime, int warnings) {
-        long numberOfTenantsToProcess = tenantRepository.countByLastLoginDateIsBeforeAndHasDocuments(localDateTime, warnings);
-        if (numberOfTenantsToProcess == 0) {
-            return;
-        }
-        int lengthOfPage = 100;
-        int numberOfPage = (int) (numberOfTenantsToProcess / lengthOfPage);
-        for (int i = numberOfPage; i >= 0; i--) {
-            Pageable page = PageRequest.of(i, lengthOfPage, Sort.Direction.DESC, "id");
-            processWarningsBatch(localDateTime, warnings, page);
-        }
-    }
-
-    private void processWarningsBatch(LocalDateTime localDateTime, int warnings, Pageable page) {
-        Page<Tenant> tenantList = tenantRepository.findByLastLoginDateIsBeforeAndHasDocuments(page, localDateTime, warnings);
-
+        Pageable pageable = PageRequest.of(0, PAGE_SIZE, Sort.Direction.DESC, "id");
+        Page<Tenant> tenantPage = tenantRepository.findByLastLoginDateIsBeforeAndHasDocuments(pageable, localDateTime, warnings);
         switch (warnings) {
-            case 0 ->
-                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants who will be warned for FIRST time by email");
-            case 1 ->
-                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants who will be warned for SECOND time by email");
-            case 2 ->
-                    log.info("accountWarnings. Found [" + tenantList.getTotalElements() + "] tenants whose documents will be deleted");
+            case 0 -> log.info("Found {} tenants who will be warned for FIRST time by email", tenantPage.getTotalElements());
+            case 1 -> log.info("Found {} tenants who will be warned for SECOND time by email", tenantPage.getTotalElements());
+            case 2 -> log.info("Found {} tenants whose documents will be deleted", tenantPage.getTotalElements());
         }
 
-        tenantList.stream()
-                .filter(tenant -> isNotBlank(tenant.getEmail()))
-                .forEach(t -> {
-            try {
-                tenantWarningService.handleTenantWarning(t, warnings);
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                Sentry.captureException(e);
-            }
-        });
+        processWarningsForPage(warnings, tenantPage);
+
+        while (tenantPage.hasNext()) {
+            tenantPage = tenantRepository.findByLastLoginDateIsBeforeAndHasDocuments(tenantPage.nextPageable(), localDateTime, warnings);
+            processWarningsForPage(warnings, tenantPage);
+        }
     }
 
+    private void processWarningsForPage(int warnings, Page<Tenant> tenantPage) {
+        tenantPage.stream()
+                .filter(tenant -> isNotBlank(tenant.getEmail()))
+                .forEach(t -> tryHandlingTenantWarning(warnings, t));
+    }
+
+    private void archiveCotenantAccounts() {
+        Pageable pageable = PageRequest.of(0, PAGE_SIZE, Sort.Direction.DESC, "id");
+        Page<Tenant> tenantPage = tenantRepository.findCotenantsWithNoEmailAndArchivedMainTenant(pageable);
+
+        tenantPage.stream().forEach(t -> tryHandlingTenantWarning(2, t));
+
+        while (tenantPage.hasNext()) {
+            tenantPage = tenantRepository.findCotenantsWithNoEmailAndArchivedMainTenant(tenantPage.nextPageable());
+            tenantPage.stream().forEach(t -> tryHandlingTenantWarning(2, t));
+        }
+    }
+
+    private void tryHandlingTenantWarning(int warnings, Tenant t) {
+        try {
+            tenantWarningService.handleTenantWarning(t, warnings);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 
 }
