675cae2eae2338ad04ef757f6823fde48d1d4e24
Juliette de Rancourt <juliette@dossierfacile.fr>
2023-12-04 10:31:34 +0100

    feat(tasks): Delete all inactive tenants found during TENANT_DELETION task iteration

--- dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/tenantwarning/TenantDeletionTask.java
+++ dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/tenantwarning/TenantDeletionTask.java
@@ -4,19 +4,22 @@ import fr.dossierfacile.scheduler.LoggingContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import static fr.dossierfacile.scheduler.tasks.TaskName.TENANT_DELETION;
+import static java.time.LocalDateTime.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class TenantDeletionTask {
+
+    private static final int PAGE_SIZE = 1000;
 
     @Value("${months_for_deletion_of_archived_tenants:12}")
     private Integer monthsForDeletionOfTenants;
@@ -26,16 +29,27 @@ public class TenantDeletionTask {
     @Scheduled(cron = "${cron.account-deletion}")
     private void deleteOldAccounts() {
         LoggingContext.startTask(TENANT_DELETION);
-        List<Long> tenantIdsToDelete = getTenantsToDelete();
-        tenantIdsToDelete.forEach(this::deleteTenant);
-        log.info("Deleted {} inactive old tenants", tenantIdsToDelete.size());
+
+        LocalDateTime limitDate = now().minusMonths(monthsForDeletionOfTenants);
+        deleteTenantsNotActiveSince(limitDate);
+
         LoggingContext.endTask();
     }
 
-    private List<Long> getTenantsToDelete() {
-        LocalDateTime limitDate = LocalDateTime.now().minusMonths(monthsForDeletionOfTenants);
-        PageRequest pageRequest = PageRequest.of(0, 5000);
-        return tenantRepository.findByLastUpdateDate(limitDate, pageRequest);
+    private void deleteTenantsNotActiveSince(LocalDateTime limitDate) {
+        Page<Long> toDelete = tenantRepository.findByLastUpdateDate(limitDate, PageRequest.of(0, PAGE_SIZE));
+        log.info("Found {} inactive tenants to delete ({} months old)", toDelete.getTotalElements(), monthsForDeletionOfTenants);
+
+        deleteAllTenants(toDelete);
+
+        while (toDelete.hasNext()) {
+            toDelete = tenantRepository.findByLastUpdateDate(limitDate, toDelete.nextPageable());
+            deleteAllTenants(toDelete);
+        }
+    }
+
+    private void deleteAllTenants(Page<Long> tenantIds) {
+        tenantIds.forEach(this::deleteTenant);
     }
 
     private void deleteTenant(Long tenantId) {
