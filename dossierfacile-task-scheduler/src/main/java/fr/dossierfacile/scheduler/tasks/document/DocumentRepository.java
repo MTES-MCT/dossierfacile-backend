d80a6d90cda1b47455289d6683cd208322159a34
Fabien <fabien@dossierfacile.fr>
2023-12-21 22:57:41 +0100

    feat: clean failed pdf - synchronize partner and send mail

--- dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/document/DocumentRepository.java
+++ dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/tasks/document/DocumentRepository.java
@@ -1,4 +1,4 @@
-package fr.dossierfacile.scheduler.tasks.documenttoprocess;
+package fr.dossierfacile.scheduler.tasks.document;
 
 import fr.dossierfacile.common.entity.Document;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,13 +14,24 @@ public interface DocumentRepository extends JpaRepository<Document, Long> {
     @Query(value = """
             SELECT *
             FROM document
-            WHERE document_status = 'TO_PROCESS'
-              AND watermark_file_id IS NULL
+            WHERE watermark_file_id IS NULL
               AND (last_modified_date IS NULL OR last_modified_date < :to)
             ORDER BY
               CASE WHEN last_modified_date IS NULL THEN 1 ELSE 0 END,
               last_modified_date DESC
             LIMIT 200;
             """, nativeQuery = true)
-    List<Document> findToProcessWithoutPDFToDate(@Param("to") LocalDateTime toDateTime);
+    List<Document> findWithoutPDFToDate(@Param("to") LocalDateTime toDateTime);
+
+    @Query("""
+            SELECT d
+            FROM Document d
+            LEFT JOIN FETCH d.guarantor g
+            WHERE d.watermarkFile IS NULL 
+            AND ((d.lastModifiedDate IS NOT NULL AND d.lastModifiedDate < :to) 
+            OR (d.lastModifiedDate IS NULL AND d.creationDateTime < :to))
+            ORDER BY d.lastModifiedDate DESC
+            """)
+    List<Document> findDocumentWithoutPDFToDate(@Param("to") LocalDateTime toDateTime);
+
 }
