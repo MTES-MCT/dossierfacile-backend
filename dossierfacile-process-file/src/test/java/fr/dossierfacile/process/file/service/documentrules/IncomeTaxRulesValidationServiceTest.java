116c8b547412fba54546b4273c67935014bc4d31
Fabien <fabien@dossierfacile.fr>
2024-01-05 12:31:45 +0100

    fix: analyse previously existing document

--- dossierfacile-process-file/src/test/java/fr/dossierfacile/process/file/service/documentrules/IncomeTaxRulesValidationServiceTest.java
+++ dossierfacile-process-file/src/test/java/fr/dossierfacile/process/file/service/documentrules/IncomeTaxRulesValidationServiceTest.java
@@ -1,15 +1,16 @@
 package fr.dossierfacile.process.file.service.documentrules;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.dossierfacile.common.entity.*;
 import fr.dossierfacile.common.enums.DocumentCategory;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
-import fr.dossierfacile.process.file.service.ocr.GuaranteeVisaleParser;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.time.LocalDate;
 import java.util.LinkedList;
+import java.util.List;
 
 class IncomeTaxRulesValidationServiceTest {
 
@@ -17,11 +18,7 @@ class IncomeTaxRulesValidationServiceTest {
     private IncomeTaxRulesValidationService incomeTaxRulesValidationService = new IncomeTaxRulesValidationService();
 
 
-    private Document buildValidTaxDocument() throws Exception {
-        Tenant tenant = Tenant.builder()
-                .firstName("Jean")
-                .lastName("DUPONT")
-                .build();
+    private File buildValidDfFileWithYear(int year) throws JsonProcessingException {
         BarCodeFileAnalysis barCodeFileAnalysis = BarCodeFileAnalysis.builder()
                 .documentType(BarCodeDocumentType.TAX_ASSESSMENT)
                 .verifiedData(
@@ -30,24 +27,33 @@ class IncomeTaxRulesValidationServiceTest {
                                          "Déclarant 1": "DUPONT Jean",
                                          "Déclarant 2": "DUPONT Marie",
                                          "Nombre de parts": "2",
-                                         "Année des revenus": "2022",
+                                         "Année des revenus": "{2022}",
                                          "Date de mise en recouvrement": "30092023",
                                          "Revenu fiscal de référence": "42902",
                                          "Numéro fiscal du déclarant 1": "1234567890123",
                                          "Numéro fiscal du déclarant 2": "1234567890124",
                                          "Référence d’avis d’impôt": "2310A12345678"
                                        }                         
-                                """, GuaranteeVisaleParser.class)
+                                """.replace("{2022}", String.valueOf(year)), Object.class)
                 )
                 .build();
-        File dfFile = File.builder()
+        return File.builder()
                 .fileAnalysis(barCodeFileAnalysis)
                 .build();
+    }
+
+    private Document buildValidTaxDocument() throws Exception {
+        Tenant tenant = Tenant.builder()
+                .firstName("Jean")
+                .lastName("DUPONT")
+                .build();
+        List<File> files = new LinkedList<>();
+        files.add(buildValidDfFileWithYear(LocalDate.now().minusMonths(21).getYear()));
         return Document.builder()
                 .tenant(tenant)
                 .documentCategory(DocumentCategory.TAX)
                 .documentSubCategory(DocumentSubCategory.MY_NAME)
-                .files(Arrays.asList(dfFile))
+                .files(files)
                 .noDocument(true)
                 .build();
     }
@@ -63,6 +69,20 @@ class IncomeTaxRulesValidationServiceTest {
 
         Assertions.assertThat(report.getAnalysisStatus()).isEqualTo(DocumentAnalysisStatus.CHECKED);
     }
+
+    @Test
+    public void document_ok_with_two_file() throws Exception {
+        Document document = buildValidTaxDocument();
+        document.getFiles().add(buildValidDfFileWithYear(LocalDate.now().minusMonths(31).getYear()));
+        DocumentAnalysisReport report = DocumentAnalysisReport.builder()
+                .document(document)
+                .brokenRules(new LinkedList<>())
+                .build();
+        incomeTaxRulesValidationService.process(document, report);
+
+        Assertions.assertThat(report.getAnalysisStatus()).isEqualTo(DocumentAnalysisStatus.CHECKED);
+    }
+
     @Test
     public void document_full_test_wrong_firstname() throws Exception {
         Document document = buildValidTaxDocument();
