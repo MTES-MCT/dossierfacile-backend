7bca1b07f4c99b289443e3afb1f2d0e61937c1ff
Fabien <fabien@dossierfacile.fr>
2024-01-10 12:47:22 +0100

    chore: remove deprecated encryption key version - only auhtorize managed version

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/exceptions/UnsupportedKeyException.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/exceptions/UnsupportedKeyException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class OldKeyException extends RuntimeException {
+public class UnsupportedKeyException extends RuntimeException {
 
-    public OldKeyException(String message) {
+    public UnsupportedKeyException(String message) {
         super(message);
     }
 
