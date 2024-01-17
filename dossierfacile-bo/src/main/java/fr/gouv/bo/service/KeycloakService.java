a5c3b1cf2c504ce8d1be336a281c7e76f49d7f9e
Fabien <fabien@dossierfacile.fr>
2024-01-17 12:42:26 +0100

    feat(bo): create keycloak user when partners is created

--- dossierfacile-bo/src/main/java/fr/gouv/bo/service/KeycloakService.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/service/KeycloakService.java
@@ -1,6 +1,7 @@
 package fr.gouv.bo.service;
 
 import fr.dossierfacile.common.entity.User;
+import fr.dossierfacile.common.entity.UserApi;
 import org.keycloak.representations.idm.UserRepresentation;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface KeycloakService {
     void deleteKeycloakSingleUser(User tenant);
 
     void deleteKeycloakUsers(List<User> tenants);
+
+    void createKeycloakClient(UserApi userApi);
 }
