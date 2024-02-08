df8ec0b1a08a9f56684abdb37646d8fdd3269376
Juliette de Rancourt <juliette@dossierfacile.fr>
2024-02-08 15:43:36 +0100

    feat(process-file): Use ANTS trust service list to retrieve 2D-Doc signing certificates

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/validation/TwoDDocCertificationAuthorities.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/validation/TwoDDocCertificationAuthorities.java
@@ -2,68 +2,68 @@ package fr.dossierfacile.process.file.barcode.twoddoc.validation;
 
 import fr.dossierfacile.process.file.barcode.twoddoc.parsing.TwoDDocHeader;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateFactory;
+import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class TwoDDocCertificationAuthorities {
 
-    private final Map<String, X509Certificate> certificates = new HashMap<>();
+    private final AntsTrustServiceList trustServiceList;
+    private final Map<String, X509Certificate> certificatesById = new HashMap<>();
     private final RestTemplate restTemplate;
 
     public X509Certificate getCertificateUsedFor(TwoDDocHeader twoDDocHeader) {
         String certId = twoDDocHeader.certId();
-        if (!certificates.containsKey(certId)) {
+        if (!certificatesById.containsKey(certId)) {
             X509Certificate certificate = fetchCertificate(twoDDocHeader);
-            certificates.put(certId, certificate);
+            certificatesById.put(certId, certificate);
         }
-        return certificates.get(certId);
+        return certificatesById.get(certId);
     }
 
     private X509Certificate fetchCertificate(TwoDDocHeader twoDDocHeader) {
         try {
-            URI uri = getCertificateUri(twoDDocHeader);
+            URI uri = trustServiceList.getCertificateUri(twoDDocHeader);
             ResponseEntity<byte[]> entity = restTemplate.getForEntity(uri, byte[].class);
             X509Certificate certificate = parseX509Certificate(entity.getBody());
-            certificate.checkValidity();
+            checkValidity(certificate);
             return certificate;
         } catch (CertificateException e) {
             throw new TwoDDocValidationException("Invalid signing certificate for " + twoDDocHeader, e);
         }
     }
 
-    private URI getCertificateUri(TwoDDocHeader twoDDocHeader) {
-        // TODO use ANTS TLS instead of hard coded urls
-        String baseUri = switch (twoDDocHeader.issuer()) {
-            case "FR03" -> "http://certificates.certigna.fr/search.php?iHash=xvNLC1KMs03t%2FgxzdBYParPnf%2BM";
-            case "FR04" -> "http://pki-g2.ariadnext.fr/pki-2ddoc.der";
-            default -> throw new TwoDDocValidationException("Unsupported certification authority");
-        };
-        return UriComponentsBuilder.fromUriString(baseUri)
-                .queryParam("name", twoDDocHeader.certId())
-                .build().toUri();
-    }
-
     private X509Certificate parseX509Certificate(byte[] bytes) throws CertificateException {
         CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
-        try(InputStream inputStream = new ByteArrayInputStream(bytes)) {
+        try (InputStream inputStream = new ByteArrayInputStream(bytes)) {
             return (X509Certificate) certFactory.generateCertificate(inputStream);
         } catch (IOException e) {
             throw new TwoDDocValidationException(e);
         }
     }
-    
+
+    private void checkValidity(X509Certificate certificate) {
+        try {
+            certificate.checkValidity();
+        } catch (CertificateExpiredException | CertificateNotYetValidException e) {
+            log.warn("Certificate is currently not valid: {}", e.getMessage());
+        }
+    }
+
 }
