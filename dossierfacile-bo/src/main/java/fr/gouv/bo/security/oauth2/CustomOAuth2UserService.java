f457223fff44a45ac81e5fff0a9b27647a1211c2
Juliette de Rancourt <juliette@dossierfacile.fr>
2024-02-14 11:35:22 +0100

    chore: Replace deprecated spring API usages

--- dossierfacile-bo/src/main/java/fr/gouv/bo/security/oauth2/CustomOAuth2UserService.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/security/oauth2/CustomOAuth2UserService.java
@@ -55,7 +55,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     public OAuth2User processOAuth2User(OAuth2UserRequest oAuth2UserRequest, OAuth2User oAuth2User) {
         OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(oAuth2UserRequest.getClientRegistration().getRegistrationId(), oAuth2User.getAttributes());
-        if (StringUtils.isEmpty(oAuth2UserInfo.getEmail())) {
+        if (!StringUtils.hasLength(oAuth2UserInfo.getEmail())) {
             throw new OAuth2AuthenticationProcessingException("Email not found from OAuth2 provider");
         }
         if (!Objects.requireNonNull(StringUtils.split(oAuth2UserInfo.getEmail(), "@"))[1].contains(authorizeDomainBo)) {
