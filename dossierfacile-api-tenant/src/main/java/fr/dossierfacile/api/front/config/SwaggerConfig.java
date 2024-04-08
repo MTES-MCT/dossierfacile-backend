c37e1cac699a2d11586fe63ecd72307129a552a5
Fabien <fabien@dossierfacile.fr>
2024-04-08 17:13:32 +0200

    fix: swagger with springboot3

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/SwaggerConfig.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/SwaggerConfig.java
@@ -1,7 +1,7 @@
 package fr.dossierfacile.api.front.config;
 
 
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
