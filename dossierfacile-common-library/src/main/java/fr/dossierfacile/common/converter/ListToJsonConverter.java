ebdf017c8d63423605d21dd4763342c25d2c4c9e
Fabien <fabien@dossierfacile.fr>
2024-04-09 17:39:48 +0200

    fix: check null before convert

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/converter/ListToJsonConverter.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/converter/ListToJsonConverter.java
@@ -1,12 +1,13 @@
 package fr.dossierfacile.common.converter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.core.type.TypeReference;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 
 @Converter
@@ -16,6 +17,9 @@ public class ListToJsonConverter<T> implements AttributeConverter<List<T>, Strin
 
     @Override
     public String convertToDatabaseColumn(List<T> list) {
+        if (list == null) {
+            return null;
+        }
         try {
             return objectMapper.writeValueAsString(list);
         } catch (JsonProcessingException e) {
@@ -25,6 +29,9 @@ public class ListToJsonConverter<T> implements AttributeConverter<List<T>, Strin
 
     @Override
     public List<T> convertToEntityAttribute(String data) {
+        if (data == null) {
+            return Collections.emptyList();
+        }
         try {
             return objectMapper.readValue(data, new TypeReference<>() {
             });
