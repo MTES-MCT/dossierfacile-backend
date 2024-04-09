4b2d0c9abecc22c46fc242233caf6ba59fbc3bd2
Fabien <fabien@dossierfacile.fr>
2024-04-09 17:40:26 +0200

    chore: clean unused js code on BO

--- dossierfacile-bo/src/main/resources/static/js/bo.js
+++ dossierfacile-bo/src/main/resources/static/js/bo.js
@@ -4,52 +4,6 @@ $(document).ready(function () {
         $('#successMessage').fadeOut('fast');
     }, 5000); // <-- time in milliseconds
 
-    var table = $('#propertiesTable').DataTable({
-        "sAjaxSource": "/bo/properties-pro/table",
-        "sAjaxDataProp": "",
-        "order": [[5, "desc"]],
-        "aoColumns": [
-            {
-                "mRender": function (data, type, row) {
-                    if (row.floating) {
-                        return '<span class="badge" style = "background-color: red;color: red">a</span>'
-                    } else {
-                        return '';
-                    }
-                }
-            },
-            {"mData": "name"},
-            {"mData": "propertyId"},
-            {"mData": "rentCost"},
-            {"mData": "agentEmail"},
-            {"mData": "dateFormat"},
-            {"mData": "cantProspect"},
-            {
-                "mRender": function (data, type, row) {
-                    var buttons = '<a class="fa fa-pencil nounderline" href="#" ' +
-                        'data-toggle="modal" ' +
-                        'data-target="#editProperty"' +
-                        'data-name="' + row.name + '"' +
-                        'data-agent-email="' + row.agentEmail + '"' +
-                        'data-property-id="' + row.propertyId + '"' +
-                        'data-rent-cost="' + row.rentCost + '"' +
-                        'data-id="' + row.id + '"' +
-                        '></a> ' +
-                        '<a class="fa fa-trash nounderline" href="#" ' +
-                        'data-toggle="modal" ' +
-                        'data-target="#deleteProperty"' +
-                        'data-id="' + row.id + '"' +
-                        '></a>';
-                    if (row.displayed) {
-                        buttons += ' <a class="fa fa-eye-slash nounderline" href="/bo/properties-pro/display/' + row.id + '"></a>'
-                    } else {
-                        buttons += ' <a class="fa fa-eye nounderline" href="/bo/properties-pro/display/' + row.id + '"></a>'
-                    }
-                    return buttons;
-                }
-            }
-        ]
-    });
     $(function() {
         setTimeout(function() { $("#hideDiv").fadeOut(1500); }, 4000)
 
@@ -67,14 +21,9 @@ $(document).ready(function () {
             "dataSrc": function (response) {
 
                 var data = response.data; // your data list
-
                 var all = [];
-
                 for (var i = 0; i < data.length; i++) {
-
-
                     var row = {
-
                         rows: response.start + i + 1,
                         id: data[i].id,
                         userApiId: data[i].userApiId,
