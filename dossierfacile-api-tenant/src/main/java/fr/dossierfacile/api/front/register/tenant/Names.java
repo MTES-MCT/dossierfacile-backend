511d9a19fcbfbb30714f7bc694e50c327ae4e121
Fabien <fabien@dossierfacile.fr>
2024-05-03 14:47:46 +0200

    fix: reset organism/legal guarantor documents on tenant name change

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/register/tenant/Names.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/register/tenant/Names.java
@@ -7,16 +7,21 @@ import fr.dossierfacile.api.front.register.form.tenant.NamesForm;
 import fr.dossierfacile.api.front.service.interfaces.ApartmentSharingService;
 import fr.dossierfacile.api.front.service.interfaces.DocumentService;
 import fr.dossierfacile.api.front.service.interfaces.TenantStatusService;
+import fr.dossierfacile.common.entity.Document;
 import fr.dossierfacile.common.entity.Tenant;
 import fr.dossierfacile.common.enums.DocumentCategory;
+import fr.dossierfacile.common.enums.TypeGuarantor;
 import fr.dossierfacile.common.repository.TenantCommonRepository;
 import lombok.AllArgsConstructor;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -35,7 +40,13 @@ public class Names implements SaveStep<NamesForm> {
         if (!StringUtils.equals(tenant.getFirstName(), namesForm.getFirstName())
                 || !StringUtils.equals(tenant.getLastName(), namesForm.getLastName())
                 || !StringUtils.equals(tenant.getPreferredName(), namesForm.getPreferredName())) {
-            documentService.resetValidatedOrInProgressDocumentsAccordingCategories(tenant.getDocuments(), Arrays.asList(DocumentCategory.values()));
+            List<Document> documentsToCheck = new ArrayList<>(tenant.getDocuments());
+            if (CollectionUtils.isNotEmpty(tenant.getGuarantors())
+                    && (tenant.getGuarantors().getFirst().getTypeGuarantor() == TypeGuarantor.LEGAL_PERSON
+                    || tenant.getGuarantors().getFirst().getTypeGuarantor() == TypeGuarantor.ORGANISM)) {
+                documentsToCheck.addAll(tenant.getGuarantors().getFirst().getDocuments());
+            }
+            documentService.resetValidatedOrInProgressDocumentsAccordingCategories(documentsToCheck, Arrays.asList(DocumentCategory.values()));
         }
         if (!tenant.getFranceConnect()) {
             tenant.setFirstName(namesForm.getFirstName());
