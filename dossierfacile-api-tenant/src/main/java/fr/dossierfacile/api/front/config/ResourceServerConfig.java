aa355c3d8740d0856784833744585f44d4a9fcda
Fabien <fabien@dossierfacile.fr>
2024-05-06 11:36:33 +0200

    Revert "chore: enable CSRF - only for security scanning purpose"
    This reverts commit 839a4eb701ff6124d6c0fe0b1e191f167fe9b913.

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/ResourceServerConfig.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/ResourceServerConfig.java
@@ -46,6 +46,7 @@ public class ResourceServerConfig {
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(withDefaults())
                 .authorizeHttpRequests(auth -> auth
