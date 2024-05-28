f103bb75cd61ba6a78e0a67abf0e17b2e3040fb7
Fabien <fabien@dossierfacile.fr>
2024-05-28 16:01:40 +0200

    chore: remove unused code

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/BOApartmentSharingRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/BOApartmentSharingRepository.java
@@ -4,9 +4,4 @@ import fr.dossierfacile.common.entity.ApartmentSharing;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BOApartmentSharingRepository extends JpaRepository<ApartmentSharing, Long> {
-    
-    ApartmentSharing findOneByToken(String token);
-
-    ApartmentSharing findOneByTokenPublic(String token);
-
 }
