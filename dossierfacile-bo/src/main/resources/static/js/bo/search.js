6228ef6ccb2c3903787fc604f58d1bbc169f0c2e
Fabien <fabien@dossierfacile.fr>
2024-06-12 13:20:30 +0200

    feat: highlight tenant search result

---
+++ dossierfacile-bo/src/main/resources/static/js/bo/search.js
@@ -0,0 +1,10 @@
+ document.addEventListener('DOMContentLoaded', function () {
+            const keySearch = document.getElementById('search-text').innerText;
+            document.querySelectorAll('.to-highlight')
+            const elementsToHighlight = document.querySelectorAll('.to-highlight');
+            const regex = new RegExp(`${keySearch}`, 'gi');
+            elementsToHighlight.forEach(element => {
+                console.log(element);
+                element.innerHTML = element.innerHTML.replace(regex, "<span class='highlight'>$&</span>");
+            });
+        });
