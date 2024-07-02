4599c14fa43e11b01c4c425156b0c22e483bafae
Fabien <fabien@dossierfacile.fr>
2024-07-02 17:38:49 +0200

    fix: force image codecs to be loaded

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/ProcessFileApplication.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/ProcessFileApplication.java
@@ -1,5 +1,6 @@
 package fr.dossierfacile.process.file;
 
+import fr.dossierfacile.common.config.ImageIOInitializer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +24,7 @@ public class ProcessFileApplication {
 
     public static void main(String[] args) {
         //System.setProperty("jna.library.path", "/usr/local/lib");
+        ImageIOInitializer.initialize();
         SpringApplication.run(ProcessFileApplication.class, args);
         int mb = 1024 * 1024;
         MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
