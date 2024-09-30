6e1aff12dc1b9614fadbb10b48408d416d4a2bab
Fabien <fabien@dossierfacile.fr>
2024-09-30 11:41:33 +0200

    chore: remove useless dependency - io.jsontoken

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/validator/tenant/application/CoTenantsEmailRequiredForGroupValidator.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/validator/tenant/application/CoTenantsEmailRequiredForGroupValidator.java
@@ -3,12 +3,11 @@ package fr.dossierfacile.api.front.validator.tenant.application;
 import fr.dossierfacile.api.front.register.form.tenant.ApplicationFormV2;
 import fr.dossierfacile.api.front.validator.annotation.tenant.application.v2.CoTenantsEmailRequiredForGroup;
 import fr.dossierfacile.common.enums.ApplicationType;
-import io.jsonwebtoken.lang.Collections;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-
+import org.springframework.util.CollectionUtils;
 
 @RequiredArgsConstructor
 public class CoTenantsEmailRequiredForGroupValidator implements ConstraintValidator<CoTenantsEmailRequiredForGroup, ApplicationFormV2> {
@@ -20,7 +19,7 @@ public class CoTenantsEmailRequiredForGroupValidator implements ConstraintValida
 
     @Override
     public boolean isValid(ApplicationFormV2 applicationForm, ConstraintValidatorContext constraintValidatorContext) {
-        if (applicationForm.getApplicationType() != ApplicationType.GROUP || Collections.isEmpty(applicationForm.getCoTenants())) {
+        if (applicationForm.getApplicationType() != ApplicationType.GROUP || CollectionUtils.isEmpty(applicationForm.getCoTenants())) {
             return true;
         }
         return !applicationForm.getCoTenants().stream().anyMatch(t -> StringUtils.isBlank(t.getEmail()));
