e9a841b84c1087ee86813eb5d7857acbf97dd095
Fabien <fabien@dossierfacile.fr>
2024-10-08 16:58:29 +0200

    chore: delete duplicate models

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/model/dfc/apartment_sharing/TenantModel.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/model/dfc/apartment_sharing/TenantModel.java
@@ -2,6 +2,7 @@ package fr.dossierfacile.api.front.model.dfc.apartment_sharing;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import fr.dossierfacile.api.front.model.tenant.DocumentModel;
+import fr.dossierfacile.api.front.model.tenant.GuarantorModel;
 import fr.dossierfacile.common.enums.TenantFileStatus;
 import fr.dossierfacile.common.enums.TenantType;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class TenantModel {
     private Long id;
