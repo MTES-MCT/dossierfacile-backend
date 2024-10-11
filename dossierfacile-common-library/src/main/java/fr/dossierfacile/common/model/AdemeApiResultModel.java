82ccc44113ce6a2217bb8097511585923848a898
Matthieu Bollot <m.bollot@code-troopers.com>
2024-10-11 14:09:55 +0200

    ✨ Feat(scheduler): regularly check api ademe and log errors

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/AdemeApiResultModel.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/AdemeApiResultModel.java
@@ -1,4 +1,4 @@
-package fr.dossierfacile.api.dossierfacileapiowner.property;
+package fr.dossierfacile.common.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
