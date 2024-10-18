9f3fa279ffd73a8b2146625610e9a73acd0a426d
Matthieu Bollot <m.bollot@code-troopers.com>
2024-10-18 10:06:47 +0200

    ✨ Feat: handle heic files

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/service/DocumentHelperServiceImpl.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/service/DocumentHelperServiceImpl.java
@@ -11,13 +11,13 @@ import fr.dossierfacile.common.utils.FileUtility;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -26,6 +26,8 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
@@ -42,15 +44,30 @@ public class DocumentHelperServiceImpl implements DocumentHelperService {
     @Transactional
     @Override
     public File addFile(MultipartFile multipartFile, Document document) throws IOException {
-
         StorageFile storageFile = StorageFile.builder()
-                .name(multipartFile.getOriginalFilename())
-                .contentType(multipartFile.getContentType())
                 .size(multipartFile.getSize())
                 .encryptionKey(encryptionKeyService.getCurrentKey())
                 .build();
+        String originalFilename = multipartFile.getOriginalFilename();
+        if (originalFilename == null) {
+            originalFilename = UUID.randomUUID().toString();
+        }
+        if ("image/heif".equals(multipartFile.getContentType())) {
+            storageFile.setName(originalFilename.replaceAll("(?i)\\.heic$", "") + ".jpg");
+            storageFile.setContentType("image/jpeg");
 
-        storageFile = fileStorageService.upload(multipartFile.getInputStream(), storageFile);
+            InputStream jpgInputStream = convertHeicToJpg(multipartFile.getInputStream());
+            if (jpgInputStream != null) {
+                storageFile = fileStorageService.upload(jpgInputStream, storageFile);
+            } else {
+                throw new IOException("Image could not be saved");
+            }
+        } else {
+            storageFile.setName(originalFilename);
+            storageFile.setContentType(multipartFile.getContentType());
+            storageFile = fileStorageService.upload(multipartFile.getInputStream(), storageFile);
+        }
+
 
         File file = File.builder()
                 .storageFile(storageFile)
@@ -62,6 +79,40 @@ public class DocumentHelperServiceImpl implements DocumentHelperService {
             document.getFiles().add(file);
         }
         return file;
+    }
+
+    @Override
+    public InputStream convertHeicToJpg(InputStream heicInputStream) throws IOException {
+        String tmpImageName = UUID.randomUUID().toString();
+        java.io.File heicFile = java.io.File.createTempFile(tmpImageName, ".heic");
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream(heicFile)) {
+            IOUtils.copy(heicInputStream, fileOutputStream);
+        }
+
+        java.io.File jpgFile = java.io.File.createTempFile(tmpImageName, ".jpg");
+
+        // Use ImageMagick to convert .heic to .jpg
+        ProcessBuilder processBuilder = new ProcessBuilder("convert", heicFile.getAbsolutePath(), jpgFile.getAbsolutePath());
+        Process process = processBuilder.start();
+        try {
+            int exitCode = process.waitFor();
+            if (exitCode != 0) {
+                throw new IOException("Erreur lors de la conversion HEIC en JPG avec ImageMagick.");
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        InputStream jpgInputStream = new FileInputStream(jpgFile);
+
+        boolean delete = heicFile.delete();
+        if (!delete) {
+            log.error("Could not delete temporary file");
+        }
+        jpgFile.deleteOnExit();
+
+        return jpgInputStream;
     }
 
     @Override
@@ -84,7 +135,7 @@ public class DocumentHelperServiceImpl implements DocumentHelperService {
                     PDFRenderer pdfRenderer = new PDFRenderer(document);
                     BufferedImage bufferedImage = pdfRenderer.renderImageWithDPI(0, 200, ImageType.RGB);
                     preview = resizeImage(bufferedImage);
-                    log.info("resize pdf duration : " + (System.currentTimeMillis() - startTime));
+                    log.info("resize pdf duration : {}", System.currentTimeMillis() - startTime);
                 }
             } else {
                 preview = resizeImage(ImageIO.read(fileInputStream));
@@ -129,7 +180,7 @@ public class DocumentHelperServiceImpl implements DocumentHelperService {
         graphics2D.dispose();
         long endTime = System.currentTimeMillis();
         long duration = (endTime - startTime);
-        log.info("resize image duration : " + duration);
+        log.info("resize image duration : {}", duration);
         return resizedImage;
     }
 
