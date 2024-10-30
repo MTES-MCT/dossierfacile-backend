de9014f587f86a782a386766d945ae5d0d92ed9e
Fabien <fabien@dossierfacile.fr>
2024-10-30 16:37:03 +0100

    feat: allow a different support email for tenant and owner

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/filter/RateLimitingFilter.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/filter/RateLimitingFilter.java
@@ -3,9 +3,11 @@ package fr.dossierfacile.api.front.config.filter;
 import jakarta.servlet.Filter;
 import jakarta.servlet.annotation.WebFilter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!dev")
 @WebFilter("/api/register/account")
 public class RateLimitingFilter extends AbstractRateLimitingFilter implements Filter {
     @Value("${ratelimit.register.capacity}")
