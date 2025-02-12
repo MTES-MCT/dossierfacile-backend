40482ddefd3bfeb6082cb1dacfada3cfa2d39c2c
nicolasSagon <sagon.nicolas@gmail.com>
2025-02-12 09:42:45 +0100

    chore: Add controller Test on tenant api
    Add test for UserController

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/SwaggerConfig.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/SwaggerConfig.java
@@ -4,15 +4,19 @@ package fr.dossierfacile.api.front.config;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
+    @Profile("!dev")
     public GroupedOpenApi dfcOpenApi() {
         return GroupedOpenApi.builder().displayName("API DFC").group("dfc").pathsToMatch("/dfc/**").build();
     }
 
     @Bean
+    @Profile("!dev")
     public GroupedOpenApi partnerOpenApi() {
         return GroupedOpenApi.builder().displayName("API Partner").group("api-partner").pathsToMatch("/api-partner/**").build();
     }
