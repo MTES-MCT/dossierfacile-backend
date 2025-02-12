0474c20aadcb4cd68b2233005bdd6c1801c2b649
nicolasSagon <sagon.nicolas@gmail.com>
2025-02-12 13:50:40 +0100

    fix(api-tenant): Rethrow the exception annoted with Response Status
    Rethrow the exception to annoted with @ResonseStatus to allow Spring boot handle this kind of annotations

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/log/GlobalExceptionHandler.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/log/GlobalExceptionHandler.java
@@ -5,14 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
+    public ResponseEntity<String> handleException(Exception e) throws Exception {
+
         log.error("Unhandled exception: ", e);
+
+        ResponseStatus responseStatus = e.getClass().getAnnotation(ResponseStatus.class);
+        if (responseStatus != null) {
+            throw e;
+        }
+
         return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
