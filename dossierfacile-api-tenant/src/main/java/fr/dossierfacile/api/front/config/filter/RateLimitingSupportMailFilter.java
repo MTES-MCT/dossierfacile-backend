508b6d322efc9bfc80dfe5d29df0c56c5b47f4bb
nicolasSagon <sagon.nicolas@gmail.com>
2025-02-12 09:47:34 +0100

    chore: Remove @Component on filter
    The filters are annotated with @Webfilter and the app use the @ServletComponentScan so the @Webfilter and the url pattern are not used.

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/filter/RateLimitingSupportMailFilter.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/config/filter/RateLimitingSupportMailFilter.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Component
 @Profile("!dev")
 @WebFilter("/api/support/email")
 public class RateLimitingSupportMailFilter extends AbstractRateLimitingFilter implements Filter {
