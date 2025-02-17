fe2ebcc008e6ac3291cf32727b507b0d8e111554
nicolasSagon <sagon.nicolas@gmail.com>
2025-02-17 13:37:28 +0100

    chore: Refactor controller tests, create dossierfacile-common-test-lib

---
+++ dossierfacile-common-test-library/src/main/java/fr/dossierfacile/parameterizedtest/ArgumentBuilder.java
@@ -0,0 +1,20 @@
+package fr.dossierfacile.parameterizedtest;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.Named;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArgumentBuilder {
+
+    public static <T> Arguments buildArguments(String name, ControllerParameter<T> object) {
+        return Arguments.of(Named.of(name, object));
+    }
+
+    @SafeVarargs
+    public static <T> List<Arguments> buildListOfArguments(Pair<String, ControllerParameter<T>>... pairList) {
+        return Arrays.stream(pairList).map(pair -> buildArguments(pair.getLeft(), pair.getRight())).toList();
+    }
+}
