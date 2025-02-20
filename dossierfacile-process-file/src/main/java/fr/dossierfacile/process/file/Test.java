cec8f2ab0a2d4ddf0f4b66b3e17b76a443af7b58
nicolasSagon <sagon.nicolas@gmail.com>
2025-02-20 10:02:50 +0100

    test 2d matrix computer vision

---
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/Test.java
@@ -0,0 +1,133 @@
+package fr.dossierfacile.process.file;
+
+import boofcv.alg.misc.ImageStatistics;
+import boofcv.alg.misc.PixelMath;
+import boofcv.alg.template.TemplateMatching;
+import boofcv.alg.template.TemplateMatchingIntensity;
+import boofcv.factory.template.FactoryTemplateMatching;
+import boofcv.factory.template.TemplateScoreType;
+import boofcv.io.UtilIO;
+import boofcv.io.image.ConvertBufferedImage;
+import boofcv.io.image.UtilImageIO;
+import boofcv.struct.feature.Match;
+import boofcv.struct.image.GrayF32;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * Example of how to find objects inside an image using template matching. Template matching works
+ * well when there is little noise in the image and the object's appearance is known and static. It can
+ * also be very slow to compute, depending on the image and template size.
+ *
+ * @author Peter Abeles
+ */
+public class Test {
+    /**
+     * Demonstrates how to search for matches of a template inside an image
+     *
+     * @param image Image being searched
+     * @param template Template being looked for
+     * @param mask Mask which determines the weight of each template pixel in the match score
+     * @param expectedMatches Number of expected matches it hopes to find
+     * @return List of match location and scores
+     */
+    private static List<Match> findMatches(GrayF32 image, GrayF32 template, GrayF32 mask,
+                                           int expectedMatches ) {
+        // create template matcher.
+        TemplateMatching<GrayF32> matcher =
+                FactoryTemplateMatching.createMatcher(TemplateScoreType.SUM_SQUARE_ERROR, GrayF32.class);
+
+        // Find the points which match the template the best
+        matcher.setImage(image);
+        matcher.setTemplate(template, mask, expectedMatches);
+        matcher.process();
+
+        return matcher.getResults().toList();
+    }
+
+    /**
+     * Computes the template match intensity image and displays the results. Brighter intensity indicates
+     * a better match to the template.
+     */
+    private static void showMatchIntensity( GrayF32 image, GrayF32 template, GrayF32 mask ) {
+        // create algorithm for computing intensity image
+        TemplateMatchingIntensity<GrayF32> matchIntensity =
+                FactoryTemplateMatching.createIntensity(TemplateScoreType.SUM_SQUARE_ERROR, GrayF32.class);
+
+        // apply the template to the image
+        matchIntensity.setInputImage(image);
+        matchIntensity.process(template, mask);
+
+        // get the results
+        GrayF32 intensity = matchIntensity.getIntensity();
+
+        // White will indicate a good match and black a bad match, or the reverse
+        // depending on the cost function used.
+        float min = ImageStatistics.min(intensity);
+        float max = ImageStatistics.max(intensity);
+        float range = max - min;
+        PixelMath.plus(intensity, -min, intensity);
+        PixelMath.divide(intensity, range, intensity);
+        PixelMath.multiply(intensity, 255.0f, intensity);
+
+        BufferedImage output = new BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_BGR);
+        UtilImageIO.saveImage(output, "/Users/nsagon/TestDataMatrix/match_intensity.png");
+    }
+
+    public static void main( String[] args ) {
+        // Load image and templates
+        String directory = "/Users/nsagon/TestDataMatrix/";
+
+        GrayF32 image = UtilImageIO.loadImage(directory, "2ddoc.jpg", GrayF32.class);
+        GrayF32 templateCursor = UtilImageIO.loadImage(directory, "template_2d_doc.png", GrayF32.class);
+        GrayF32 mask = UtilImageIO.loadImage(directory, "mask_2d_doc.png", GrayF32.class);
+
+        // create output image to show results
+        var output = new BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_BGR);
+        ConvertBufferedImage.convertTo(image, output);
+        Graphics2D g2 = output.createGraphics();
+
+        // Search for the cursor in the image. For demonstration purposes it has been pasted 3 times
+        g2.setColor(Color.RED);
+        g2.setStroke(new BasicStroke(5));
+        drawRectangles(g2, image, templateCursor, mask, 1);
+        // show match intensity image for this template
+        showMatchIntensity(image, templateCursor, mask);
+
+        UtilImageIO.saveImage(output, "/Users/nsagon/TestDataMatrix/result.png");
+    }
+
+    /**
+     * Helper function will is finds matches and displays the results as colored rectangles
+     */
+    private static void drawRectangles( Graphics2D g2,
+                                        GrayF32 image, GrayF32 template, GrayF32 mask,
+                                        int expectedMatches ) {
+        List<Match> found = findMatches(image, template, mask, expectedMatches);
+
+        int r = 2;
+        int w = template.width + 2*r;
+        int h = template.height + 2*r;
+
+        for (Match m : found) {
+            System.out.printf("Match %3d %3d    score = %6.2f\n", m.x, m.y, m.score);
+            // this demonstrates how to filter out false positives
+            // the meaning of score will depend on the template technique
+//			if( m.score < -5 )  // This line is commented out for demonstration purposes
+//				continue;
+
+            // the return point is the template's top left corner
+            int x0 = m.x - r;
+            int y0 = m.y - r;
+            int x1 = x0 + w;
+            int y1 = y0 + h;
+
+            g2.drawLine(x0, y0, x1, y0);
+            g2.drawLine(x1, y0, x1, y1);
+            g2.drawLine(x1, y1, x0, y1);
+            g2.drawLine(x0, y1, x0, y0);
+        }
+    }
+}
