MTES-MCT/Dossier-Facile-API

chore(task-scheduler): add aggregated logs for task scheduler

dossierfacile-common-library/src/main/java/fr/dossierfacile/common/utils/LoggerUtil.java
@@ -4,11 +4,14 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.LoggingEvent;
+import fr.dossierfacile.common.log.LogModel;
 import fr.dossierfacile.common.model.JobContext;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -20,29 +23,37 @@ public class LoggerUtil {
     private static final String UUID_REGEX = "(?<=/)([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})(?=[/?]|$)";
     private static final String EMAIL_REGEX = "(?<=/)([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})(?=[/?]|$)";
 
-    private static final String URI = "uri";
-    private static final String NORMALIZED_URI = "normalized_uri";
-    private static final String RESPONSE_STATUS = "response_status";
-    private static final String REAL_IP = "ip";
-    private static final String BODY = "body";
-    private static final String FORM_PARAMETERS = "form_parameters";
-    private static final String PARAMS = "params";
-    private static final String EXECUTION_TIME = "execution_time";
-    private static final String LOGS = "logs";
-    private static final String RESPONSE_CONTENT_TYPE = "response_content_type";
-    private static final String RESPONSE_BODY = "response_body";
+    private static final String API_URL = "uri";
+    private static final String API_NORMALIZED_URI = "normalized_uri";
+    private static final String API_RESPONSE_STATUS = "response_status";
+    private static final String API_REAL_IP = "ip";
+    private static final String API_BODY = "body";
+    private static final String API_FORM_PARAMETERS = "form_parameters";
+    private static final String API_PARAMS = "params";
+    private static final String API_RESPONSE_CONTENT_TYPE = "response_content_type";
+    private static final String API_RESPONSE_BODY = "response_body";
 
-    private static final String QUEUE_NAME = "queue_name";
-    private static final String FILE_ID = "file_id";
-    private static final String DOCUMENT_ID = "document_id";
-    private static final String ACTION = "action";
-    private static final String EXECUTION_START = "execution_start";
-    private static final String JOB_STATUS = "job_status";
+    private static final String PROCESS_QUEUE_NAME = "queue_name";
+    private static final String PROCESS_FILE_ID = "file_id";
+    private static final String PROCESS_DOCUMENT_ID = "document_id";
+    private static final String PROCESS_ACTION = "action";
+    private static final String PROCESS_EXECUTION_START = "execution_start";
+    private static final String PROCESS_JOB_STATUS = "job_status";
+
+    private static final String TASK_NAME = "task_name";
+    private static final String TASK_STATUS = "task_status";
+
+    private static final String LOGS = "logs";
+    private static final String EXECUTION_TIME = "execution_time";
 
 
     public static void sendEnrichedLogs(Level level, String message) {
         var rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         sendEnrichedLogs(rootLogger, level, message);
+    }
+
+    public static Level getLogLevel(List<LogModel> logs) {
+        return logs.stream().map(LogModel::getLevel).max(Comparator.comparingInt(Level::toInt)).orElse(Level.INFO);
     }
 
     public static void sendEnrichedLogs(Logger logger, Level level, String message) {
@@ -74,48 +85,55 @@ public class LoggerUtil {
     }
 
     public static void prepareMDCForHttpRequest(HttpServletRequest request, Map<String, String> additionalContextElements) {
-        MDC.put(URI, request.getRequestURI());
-        MDC.put(NORMALIZED_URI, LoggerUtil.normalizeUrl(request.getRequestURI()));
+        MDC.put(API_URL, request.getRequestURI());
+        MDC.put(API_NORMALIZED_URI, LoggerUtil.normalizeUrl(request.getRequestURI()));
         MDC.put(REQUEST_ID, UUID.randomUUID().toString());
-        MDC.put(REAL_IP, request.getHeader("X-Real-Ip")); // specific to Scalingo infra
+        MDC.put(API_REAL_IP, request.getHeader("X-Real-Ip")); // specific to Scalingo infra
         additionalContextElements.forEach(MDC::put);
     }
 
     public static void prepareMDCForWorker(JobContext jobContext, String actionType) {
         var elapsedTime = System.currentTimeMillis() - jobContext.getStartTime();
-        MDC.put(FILE_ID, String.valueOf(jobContext.getFileId()));
-        MDC.put(ACTION, actionType);
-        MDC.put(EXECUTION_START, String.valueOf(jobContext.getStartTime()));
+        MDC.put(PROCESS_FILE_ID, String.valueOf(jobContext.getFileId()));
+        MDC.put(PROCESS_ACTION, actionType);
+        MDC.put(PROCESS_EXECUTION_START, String.valueOf(jobContext.getStartTime()));
         MDC.put(EXECUTION_TIME, String.valueOf(elapsedTime));
-        MDC.put(DOCUMENT_ID, String.valueOf(jobContext.getDocumentId()));
-        MDC.put(QUEUE_NAME, jobContext.getQueueName().name());
+        MDC.put(PROCESS_DOCUMENT_ID, String.valueOf(jobContext.getDocumentId()));
+        MDC.put(PROCESS_QUEUE_NAME, jobContext.getQueueName().name());
+    }
+
+    public static void prepareMDCForScheduledTask(String taskName) {
+        var startTime = String.valueOf(System.currentTimeMillis());
+        MDC.put(PROCESS_ID, UUID.randomUUID().toString());
+        MDC.put(EXECUTION_TIME, startTime);
+        MDC.put(TASK_NAME, taskName);
     }
 
     public static void addRequestStatusToMdc(int status) {
-        MDC.put(RESPONSE_STATUS, String.valueOf(status));
+        MDC.put(API_RESPONSE_STATUS, String.valueOf(status));
     }
 
     public static void addRequestBody(String body) {
-        MDC.put(BODY, String.valueOf(body));
+        MDC.put(API_BODY, String.valueOf(body));
     }
 
     public static void addRequestParams(String params) {
-        MDC.put(PARAMS, String.valueOf(params));
+        MDC.put(API_PARAMS, String.valueOf(params));
     }
 
     public static void addRequestFormParameters(String parameters) {
-        MDC.put(FORM_PARAMETERS, String.valueOf(parameters));
+        MDC.put(API_FORM_PARAMETERS, String.valueOf(parameters));
     }
 
     public static void addResponseContentType(String contentType) {
-        MDC.put(RESPONSE_CONTENT_TYPE, String.valueOf(contentType));
+        MDC.put(API_RESPONSE_CONTENT_TYPE, String.valueOf(contentType));
     }
 
     public static void addResponseBody(String body) {
-        MDC.put(RESPONSE_BODY, String.valueOf(body));
+        MDC.put(API_RESPONSE_BODY, String.valueOf(body));
     }
 
-    public static void addElapsedTime(long responseTime) {
+    public static void addExecutionTime(long responseTime) {
         MDC.put(EXECUTION_TIME, String.valueOf(responseTime));
     }
     public static String getExecutionTime() {
@@ -126,8 +144,8 @@ public class LoggerUtil {
         MDC.put(PROCESS_ID, String.valueOf(processId));
     }
 
-    public static String getQueueName() {
-        return MDC.get(QUEUE_NAME);
+    public static String getProcessQueueName() {
+        return MDC.get(PROCESS_QUEUE_NAME);
     }
 
     public static void addLogs(String logs) {
@@ -138,16 +156,28 @@ public class LoggerUtil {
         return MDC.get(REQUEST_ID);
     }
 
+    public static String getProcessId() {
+        return MDC.get(PROCESS_ID);
+    }
+
     public static void addJobStatus(String jobStatus) {
-        MDC.put(JOB_STATUS, jobStatus);
+        MDC.put(PROCESS_JOB_STATUS, jobStatus);
     }
 
-    public static String getJobStatus() {
-        return MDC.get(JOB_STATUS);
+    public static String getProcessJobStatus() {
+        return MDC.get(PROCESS_JOB_STATUS);
     }
 
-    public static String getAction() {
-        return MDC.get(ACTION);
+    public static String getProcessAction() {
+        return MDC.get(PROCESS_ACTION);
+    }
+
+    public static void addTaskStatus(String taskStatus) {
+        MDC.put(TASK_STATUS, taskStatus);
+    }
+
+    public static String getTaskName() {
+        return MDC.get(TASK_NAME);
     }
 
     public static void clearMDC() {

dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/log/LogAggregator.java
@@ -1,24 +1,16 @@
-package fr.dossierfacile.process.file.log;
+package fr.dossierfacile.scheduler.log;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.dossierfacile.common.log.CustomAppender;
-import fr.dossierfacile.common.log.LogModel;
-import fr.dossierfacile.common.model.JobContext;
-import fr.dossierfacile.common.model.JobStatus;
 import fr.dossierfacile.common.utils.LoggerUtil;
-import fr.dossierfacile.process.file.amqp.ActionType;
 import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Comparator;
-
 @Component
-@Slf4j
 public class LogAggregator {
 
     private Logger rootLogger;
@@ -37,39 +29,34 @@ public class LogAggregator {
         rootLogger.addAppender(customAppender);
     }
 
-    public void sendWorkerLogs(JobContext jobContext, ActionType actionType) {
-        LoggerUtil.prepareMDCForWorker(jobContext, actionType.name());
-        var logs = customAppender.getLogsForUniqueIdentifier(jobContext.getProcessId());
-
+    public void sendLogs() {
+        var processId = LoggerUtil.getProcessId();
+        var logs = customAppender.getLogsForUniqueIdentifier(processId);
         try {
             var logMessages = objectMapper.writeValueAsString(logs);
             LoggerUtil.addLogs(logMessages);
         } catch (JsonProcessingException e) {
-            log.error("Error while sending worker logs", e);
+            throw new RuntimeException(e);
         }
 
-        var jobStatus = jobContext.getJobStatus();
-        if (jobStatus == null) {
-            if (logs.stream().anyMatch(log -> log.getLevel() == Level.ERROR)) {
-                LoggerUtil.addJobStatus(JobStatus.ERROR.name());
-            } else {
-                LoggerUtil.addJobStatus(JobStatus.SUCCESS.name());
-            }
-        }
+        TaskStatus taskStatus = logs.stream().anyMatch(item -> item.getLevel() == Level.ERROR) ? TaskStatus.ERROR : TaskStatus.SUCCESS;
+        LoggerUtil.addTaskStatus(taskStatus.name());
+
+        var currentTime = System.currentTimeMillis();
+        var executionTime = currentTime - Long.parseLong(LoggerUtil.getExecutionTime());
+
+        LoggerUtil.addExecutionTime(executionTime);
 
         String enrichedLogs = String.format(
-                "Action %s on Queue %s completed with status %s in %s ms",
-                LoggerUtil.getProcessAction(),
-                LoggerUtil.getProcessQueueName(),
-                LoggerUtil.getProcessJobStatus(),
-                LoggerUtil.getExecutionTime()
+                "Task %s completed with status %s in %s ms",
+                LoggerUtil.getTaskName(),
+                taskStatus.name(),
+                executionTime
         );
 
         Level logLevel = LoggerUtil.getLogLevel(logs);
         LoggerUtil.sendEnrichedLogs(rootLogger, logLevel, enrichedLogs);
 
-        customAppender.clearLogsForRequest(jobContext.getProcessId());
         LoggerUtil.clearMDC();
     }
-
 }

MTES-MCT/dossierfacile-backend

dossierfacile-common-library/src/main/java/fr/dossierfacile/common/utils/LoggerUtil.java
@@ -4,11 +4,14 @@ import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.spi.LoggingEvent;
+import fr.dossierfacile.common.log.LogModel;
 import fr.dossierfacile.common.model.JobContext;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -20,29 +23,41 @@ public class LoggerUtil {
     private static final String UUID_REGEX = "(?<=/)([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12})(?=[/?]|$)";
     private static final String EMAIL_REGEX = "(?<=/)([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})(?=[/?]|$)";
 
-    private static final String URI = "uri";
-    private static final String NORMALIZED_URI = "normalized_uri";
-    private static final String RESPONSE_STATUS = "response_status";
-    private static final String REAL_IP = "ip";
-    private static final String BODY = "body";
-    private static final String FORM_PARAMETERS = "form_parameters";
-    private static final String PARAMS = "params";
-    private static final String EXECUTION_TIME = "execution_time";
-    private static final String LOGS = "logs";
-    private static final String RESPONSE_CONTENT_TYPE = "response_content_type";
-    private static final String RESPONSE_BODY = "response_body";
+    private static final String API_URL = "uri";
+    private static final String API_NORMALIZED_URI = "normalized_uri";
+    private static final String API_RESPONSE_STATUS = "response_status";
+    private static final String API_REAL_IP = "ip";
+    private static final String API_BODY = "body";
+    private static final String API_FORM_PARAMETERS = "form_parameters";
+    private static final String API_PARAMS = "params";
+    private static final String API_RESPONSE_CONTENT_TYPE = "response_content_type";
+    private static final String API_RESPONSE_BODY = "response_body";
 
-    private static final String QUEUE_NAME = "queue_name";
-    private static final String FILE_ID = "file_id";
-    private static final String DOCUMENT_ID = "document_id";
-    private static final String ACTION = "action";
+    private static final String PROCESS_QUEUE_NAME = "queue_name";
+    private static final String PROCESS_FILE_ID = "file_id";
+    private static final String PROCESS_DOCUMENT_ID = "document_id";
+    private static final String PROCESS_ACTION = "action";
+    private static final String PROCESS_JOB_STATUS = "job_status";
+
+    private static final String TASK_NAME = "task_name";
+    private static final String TASK_STATUS = "task_status";
+    private static final String TASK_DOCUMENT_LIST = "task_document_list";
+    private static final String TASK_STORAGE_FILE_LIST = "task_storage_file_list";
+    private static final String TASK_TENANT_LIST = "task_tenant_list";
+    private static final String TASK_OWNER_LIST = "task_owner_list";
+
+    private static final String LOGS = "logs";
+    private static final String EXECUTION_TIME = "execution_time";
     private static final String EXECUTION_START = "execution_start";
-    private static final String JOB_STATUS = "job_status";
 
 
     public static void sendEnrichedLogs(Level level, String message) {
         var rootLogger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
         sendEnrichedLogs(rootLogger, level, message);
+    }
+
+    public static Level getLogLevel(List<LogModel> logs) {
+        return logs.stream().map(LogModel::getLevel).max(Comparator.comparingInt(Level::toInt)).orElse(Level.INFO);
     }
 
     public static void sendEnrichedLogs(Logger logger, Level level, String message) {
@@ -74,60 +89,71 @@ public class LoggerUtil {
     }
 
     public static void prepareMDCForHttpRequest(HttpServletRequest request, Map<String, String> additionalContextElements) {
-        MDC.put(URI, request.getRequestURI());
-        MDC.put(NORMALIZED_URI, LoggerUtil.normalizeUrl(request.getRequestURI()));
+        MDC.put(API_URL, request.getRequestURI());
+        MDC.put(API_NORMALIZED_URI, LoggerUtil.normalizeUrl(request.getRequestURI()));
         MDC.put(REQUEST_ID, UUID.randomUUID().toString());
-        MDC.put(REAL_IP, request.getHeader("X-Real-Ip")); // specific to Scalingo infra
+        MDC.put(API_REAL_IP, request.getHeader("X-Real-Ip")); // specific to Scalingo infra
         additionalContextElements.forEach(MDC::put);
     }
 
     public static void prepareMDCForWorker(JobContext jobContext, String actionType) {
         var elapsedTime = System.currentTimeMillis() - jobContext.getStartTime();
-        MDC.put(FILE_ID, String.valueOf(jobContext.getFileId()));
-        MDC.put(ACTION, actionType);
+        MDC.put(PROCESS_FILE_ID, String.valueOf(jobContext.getFileId()));
+        MDC.put(PROCESS_ACTION, actionType);
         MDC.put(EXECUTION_START, String.valueOf(jobContext.getStartTime()));
         MDC.put(EXECUTION_TIME, String.valueOf(elapsedTime));
-        MDC.put(DOCUMENT_ID, String.valueOf(jobContext.getDocumentId()));
-        MDC.put(QUEUE_NAME, jobContext.getQueueName().name());
+        MDC.put(PROCESS_DOCUMENT_ID, String.valueOf(jobContext.getDocumentId()));
+        MDC.put(PROCESS_QUEUE_NAME, jobContext.getQueueName().name());
+    }
+
+    public static void prepareMDCForScheduledTask(String taskName) {
+        var startTime = String.valueOf(System.currentTimeMillis());
+        MDC.put(PROCESS_ID, UUID.randomUUID().toString());
+        MDC.put(EXECUTION_START, startTime);
+        MDC.put(TASK_NAME, taskName);
     }
 
     public static void addRequestStatusToMdc(int status) {
-        MDC.put(RESPONSE_STATUS, String.valueOf(status));
+        MDC.put(API_RESPONSE_STATUS, String.valueOf(status));
     }
 
     public static void addRequestBody(String body) {
-        MDC.put(BODY, String.valueOf(body));
+        MDC.put(API_BODY, String.valueOf(body));
     }
 
     public static void addRequestParams(String params) {
-        MDC.put(PARAMS, String.valueOf(params));
+        MDC.put(API_PARAMS, String.valueOf(params));
     }
 
     public static void addRequestFormParameters(String parameters) {
-        MDC.put(FORM_PARAMETERS, String.valueOf(parameters));
+        MDC.put(API_FORM_PARAMETERS, String.valueOf(parameters));
     }
 
     public static void addResponseContentType(String contentType) {
-        MDC.put(RESPONSE_CONTENT_TYPE, String.valueOf(contentType));
+        MDC.put(API_RESPONSE_CONTENT_TYPE, String.valueOf(contentType));
     }
 
     public static void addResponseBody(String body) {
-        MDC.put(RESPONSE_BODY, String.valueOf(body));
+        MDC.put(API_RESPONSE_BODY, String.valueOf(body));
     }
 
-    public static void addElapsedTime(long responseTime) {
+    public static void addExecutionTime(long responseTime) {
         MDC.put(EXECUTION_TIME, String.valueOf(responseTime));
     }
     public static String getExecutionTime() {
         return MDC.get(EXECUTION_TIME);
     }
 
+    public static String getExecutionStartTime() {
+        return MDC.get(EXECUTION_START);
+    }
+
     public static void addProcessId(String processId) {
         MDC.put(PROCESS_ID, String.valueOf(processId));
     }
 
-    public static String getQueueName() {
-        return MDC.get(QUEUE_NAME);
+    public static String getProcessQueueName() {
+        return MDC.get(PROCESS_QUEUE_NAME);
     }
 
     public static void addLogs(String logs) {
@@ -138,16 +164,44 @@ public class LoggerUtil {
         return MDC.get(REQUEST_ID);
     }
 
+    public static String getProcessId() {
+        return MDC.get(PROCESS_ID);
+    }
+
     public static void addJobStatus(String jobStatus) {
-        MDC.put(JOB_STATUS, jobStatus);
+        MDC.put(PROCESS_JOB_STATUS, jobStatus);
     }
 
-    public static String getJobStatus() {
-        return MDC.get(JOB_STATUS);
+    public static String getProcessJobStatus() {
+        return MDC.get(PROCESS_JOB_STATUS);
     }
 
-    public static String getAction() {
-        return MDC.get(ACTION);
+    public static String getProcessAction() {
+        return MDC.get(PROCESS_ACTION);
+    }
+
+    public static void addTaskStatus(String taskStatus) {
+        MDC.put(TASK_STATUS, taskStatus);
+    }
+
+    public static String getTaskName() {
+        return MDC.get(TASK_NAME);
+    }
+
+    public static void addTaskDocumentList(String documentList) {
+        MDC.put(TASK_DOCUMENT_LIST, documentList);
+    }
+
+    public static void addTaskTenantList(String tenantList) {
+        MDC.put(TASK_TENANT_LIST, tenantList);
+    }
+
+    public static void addTaskOwnerList(String ownerList) {
+        MDC.put(TASK_OWNER_LIST, ownerList);
+    }
+
+    public static void addTaskStorageFileList(String storageFileList) {
+        MDC.put(TASK_STORAGE_FILE_LIST, storageFileList);
     }
 
     public static void clearMDC() {

dossierfacile-task-scheduler/src/main/java/fr/dossierfacile/scheduler/log/LogAggregator.java
@@ -1,24 +1,16 @@
-package fr.dossierfacile.process.file.log;
+package fr.dossierfacile.scheduler.log;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.dossierfacile.common.log.CustomAppender;
-import fr.dossierfacile.common.log.LogModel;
-import fr.dossierfacile.common.model.JobContext;
-import fr.dossierfacile.common.model.JobStatus;
 import fr.dossierfacile.common.utils.LoggerUtil;
-import fr.dossierfacile.process.file.amqp.ActionType;
 import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import java.util.Comparator;
-
 @Component
-@Slf4j
 public class LogAggregator {
 
     private Logger rootLogger;
@@ -37,39 +29,34 @@ public class LogAggregator {
         rootLogger.addAppender(customAppender);
     }
 
-    public void sendWorkerLogs(JobContext jobContext, ActionType actionType) {
-        LoggerUtil.prepareMDCForWorker(jobContext, actionType.name());
-        var logs = customAppender.getLogsForUniqueIdentifier(jobContext.getProcessId());
-
+    public void sendLogs() {
+        var processId = LoggerUtil.getProcessId();
+        var logs = customAppender.getLogsForUniqueIdentifier(processId);
         try {
             var logMessages = objectMapper.writeValueAsString(logs);
             LoggerUtil.addLogs(logMessages);
         } catch (JsonProcessingException e) {
-            log.error("Error while sending worker logs", e);
+            throw new RuntimeException(e);
         }
 
-        var jobStatus = jobContext.getJobStatus();
-        if (jobStatus == null) {
-            if (logs.stream().anyMatch(log -> log.getLevel() == Level.ERROR)) {
-                LoggerUtil.addJobStatus(JobStatus.ERROR.name());
-            } else {
-                LoggerUtil.addJobStatus(JobStatus.SUCCESS.name());
-            }
-        }
+        TaskStatus taskStatus = logs.stream().anyMatch(item -> item.getLevel() == Level.ERROR) ? TaskStatus.ERROR : TaskStatus.SUCCESS;
+        LoggerUtil.addTaskStatus(taskStatus.name());
+
+        var currentTime = System.currentTimeMillis();
+        var executionTime = currentTime - Long.parseLong(LoggerUtil.getExecutionStartTime());
+
+        LoggerUtil.addExecutionTime(executionTime);
 
         String enrichedLogs = String.format(
-                "Action %s on Queue %s completed with status %s in %s ms",
-                LoggerUtil.getProcessAction(),
-                LoggerUtil.getProcessQueueName(),
-                LoggerUtil.getProcessJobStatus(),
-                LoggerUtil.getExecutionTime()
+                "Task %s completed with status %s in %s ms",
+                LoggerUtil.getTaskName(),
+                taskStatus.name(),
+                executionTime
         );
 
         Level logLevel = LoggerUtil.getLogLevel(logs);
         LoggerUtil.sendEnrichedLogs(rootLogger, logLevel, enrichedLogs);
 
-        customAppender.clearLogsForRequest(jobContext.getProcessId());
         LoggerUtil.clearMDC();
     }
-
 }