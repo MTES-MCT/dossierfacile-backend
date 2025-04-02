MTES-MCT/Dossier-Facile-API

feature(tenant): add identity on tenant

dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/register/tenant/Names.java
@@ -50,7 +50,11 @@ public class Names implements SaveStep<NamesForm> {
             }
             documentService.resetValidatedOrInProgressDocumentsAccordingCategories(documentsToCheck, Arrays.asList(DocumentCategory.values()));
         }
-        if (!tenant.getFranceConnect()) {
+        // If user is france connected, we only update the tenantFirstName and tenantLastName not user_account firstname and lastname
+        if (tenant.getFranceConnect()) {
+            tenant.setTenantFirstName(namesForm.getFirstName());
+            tenant.setTenantLastName(namesForm.getLastName());
+        } else {
             tenant.setFirstName(namesForm.getFirstName());
             tenant.setLastName(namesForm.getLastName());
         }

MTES-MCT/dossierfacile-backend

dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/register/tenant/Names.java
@@ -50,11 +50,12 @@ public class Names implements SaveStep<NamesForm> {
             }
             documentService.resetValidatedOrInProgressDocumentsAccordingCategories(documentsToCheck, Arrays.asList(DocumentCategory.values()));
         }
-        if (!tenant.getFranceConnect()) {
-            tenant.setFirstName(namesForm.getFirstName());
-            tenant.setLastName(namesForm.getLastName());
-        }
+
+        tenant.setOwnerType(namesForm.getOwnerType());
+        tenant.setFirstName(namesForm.getFirstName());
+        tenant.setLastName(namesForm.getLastName());
         tenant.setPreferredName(namesForm.getPreferredName());
+
         tenant.setZipCode(namesForm.getZipCode());
         tenant.setAbroad(namesForm.getAbroad());
         tenant.lastUpdateDateProfile(LocalDateTime.now(), null);