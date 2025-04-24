91680bc0b18ea7f9ed7a78c568808262f7393a50
nicolasSagon <sagon.nicolas@gmail.com>
2025-04-24 14:06:46 +0200

    feature(file-processor): add documents broken rules for blurry files

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/BlurryRulesValidationService.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/BlurryRulesValidationService.java
@@ -1,10 +1,10 @@
 package fr.dossierfacile.process.file.service.documentrules;
 
-import fr.dossierfacile.common.entity.Document;
-import fr.dossierfacile.common.entity.DocumentAnalysisReport;
-import fr.dossierfacile.common.entity.DocumentAnalysisStatus;
+import fr.dossierfacile.common.entity.*;
+import fr.dossierfacile.common.entity.ocr.BlurryAlgorithmType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
@@ -13,23 +13,70 @@ import org.springframework.util.CollectionUtils;
 @Slf4j
 public class BlurryRulesValidationService implements RulesValidationService {
 
+    @Value("${blurry.laplacian.threshold}")
+    private double laplacianThreshold;
+    @Value("${blurry.sobel.threshold}")
+    private double sobelThreshold;
+    @Value("${blurry.fft.threshold}")
+    private double fftThreshold;
+
     @Override
     public boolean shouldBeApplied(Document document) {
-        return !CollectionUtils.isEmpty(document.getFiles());
+        return document.getFiles().stream().allMatch(file -> file.getBlurryFileAnalysis() != null)
+                && !CollectionUtils.isEmpty(document.getFiles());
     }
 
     @Override
     public DocumentAnalysisReport process(Document document, DocumentAnalysisReport report) {
-        document.getFiles().forEach(file -> {
-
-        });
-
-//                report.getBrokenRules().add(DocumentBrokenRule.builder()
-//                        .rule(DocumentRule.R_SCHOLARSHIP_NAME)
-//                        .message(DocumentRule.R_SCHOLARSHIP_NAME.getDefaultMessage())
-//                        .build());
-        report.setAnalysisStatus(DocumentAnalysisStatus.DENIED);
+        var isBlurryDocument = false;
+        for (var file : document.getFiles()) {
+            if (isFileBlurry(file.getBlurryFileAnalysis())) {
+                isBlurryDocument = true;
+            }
+        }
+        if (isBlurryDocument) {
+            report.getBrokenRules().add(DocumentBrokenRule.builder()
+                    .rule(DocumentRule.R_BLURRY_FILE)
+                    .message(DocumentRule.R_BLURRY_FILE.getDefaultMessage())
+                    .build());
+            report.setAnalysisStatus(DocumentAnalysisStatus.DENIED);
+        }
         return report;
+    }
+
+    private boolean isFileBlurry(BlurryFileAnalysis blurryFileAnalysis) {
+        var score = 0;
+        if (checkLaplacianBlurryFile(blurryFileAnalysis)) {
+            score++;
+        }
+        if (checkSobelBlurryFile(blurryFileAnalysis)) {
+            score++;
+        }
+        if (checkFFTBlurryFile(blurryFileAnalysis)) {
+            score++;
+        }
+        return score >= 2;
+    }
+
+    private boolean checkLaplacianBlurryFile(BlurryFileAnalysis blurryFileAnalysis) {
+        var laplacianResult = blurryFileAnalysis.getBlurryResults().stream().filter(item -> item.algorithm() == BlurryAlgorithmType.LAPLACIEN).findFirst();
+        return laplacianResult
+                .filter(blurryResult -> blurryResult.score() < laplacianThreshold)
+                .isPresent();
+    }
+
+    private boolean checkSobelBlurryFile(BlurryFileAnalysis blurryFileAnalysis) {
+        var sobelResult = blurryFileAnalysis.getBlurryResults().stream().filter(item -> item.algorithm() == BlurryAlgorithmType.SOBEL).findFirst();
+        return sobelResult
+                .filter(blurryResult -> blurryResult.score() < sobelThreshold)
+                .isPresent();
+    }
+
+    private boolean checkFFTBlurryFile(BlurryFileAnalysis blurryFileAnalysis) {
+        var fftResult = blurryFileAnalysis.getBlurryResults().stream().filter(item -> item.algorithm() == BlurryAlgorithmType.FFT).findFirst();
+        return fftResult
+                .filter(blurryResult -> blurryResult.score() > fftThreshold)
+                .isPresent();
     }
 
 }
