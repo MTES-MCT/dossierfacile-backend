45aa30117ad27b7a053d781a52e539c8340dad8a
nicolasSagon <sagon.nicolas@gmail.com>
2025-04-25 15:21:11 +0200

    feature(bo): Add bo generic checkbox
    The generic checkbox car be added for ALL or for TENANT / GUARANT
    They will show at the bottom of the list

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/DocumentDeniedOptionsRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/DocumentDeniedOptionsRepository.java
@@ -3,6 +3,7 @@ package fr.gouv.bo.repository;
 import fr.dossierfacile.common.entity.DocumentDeniedOptions;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +12,18 @@ public interface DocumentDeniedOptionsRepository extends JpaRepository<DocumentD
     List<DocumentDeniedOptions> findAllByDocumentSubCategoryAndDocumentUserTypeOrderByCode(DocumentSubCategory documentSubCategory, String documentUserType);
 
     List<DocumentDeniedOptions> findAllByDocumentSubCategory(DocumentSubCategory documentSubCategory);
+
+    @Query(value = """
+            select *
+            from document_denied_options
+            where (document_user_type = 'all' or document_user_type = :documentUserType)
+              and (document_sub_category = 'UNDEFINED' or document_sub_category = :documentSubCategory)
+            ORDER BY
+                CASE
+                WHEN document_sub_category = 'UNDEFINED' THEN 1
+                ELSE 0
+            END, code
+            """, nativeQuery = true)
+    List<DocumentDeniedOptions> findAllByDocumentSubCategoryAndDocumentUserTypeIncludeGeneric(String documentSubCategory, String documentUserType);
 
 }
