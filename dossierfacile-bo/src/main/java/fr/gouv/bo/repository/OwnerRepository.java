d912b3e91f74c188d1c87d22c82cbef45a8721cb
Fabien <100640323+fabiengo@users.noreply.github.com>
2025-04-28 17:03:00 +0200

    :sparkles: feat: add firstname and lastname filter to owners listing (#943)
    Co-authored-by: Fabien <fabien@dossierfacile.fr>

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/OwnerRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/OwnerRepository.java
@@ -3,11 +3,20 @@ package fr.gouv.bo.repository;
 import fr.dossierfacile.common.entity.Owner;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    Page<Owner> findAll(Pageable pageable);
-
-    Page<Owner> findAllByEmailContaining(String email, PageRequest page);
+    @Query("""
+            SELECT o FROM Owner o
+            WHERE ((:email = '' AND o.email IS NULL) OR LOWER(o.email) LIKE %:email%)
+            AND ((:firstName = '' AND o.firstName IS NULL) OR LOWER(o.firstName) LIKE %:firstName%)
+            AND ((:lastName = '' AND o.lastName IS NULL) OR LOWER(o.lastName) LIKE %:lastName%)
+            """)
+    Page<Owner> searchOwners(
+            @Param("email") String email,
+            @Param("firstName") String firstName,
+            @Param("lastName") String lastName,
+            PageRequest page);
 }
