8fb9af08d201c6918a8d2ed82535e4db7f5a5632
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-04-28 16:13:50 +0200

    ✅ Test(bo): Add tests for dynamic months

--- dossierfacile-bo/src/test/java/fr/gouv/bo/utils/DateFormatUtilTest.java
+++ dossierfacile-bo/src/test/java/fr/gouv/bo/utils/DateFormatUtilTest.java
@@ -1,7 +1,10 @@
 package fr.gouv.bo.utils;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +23,35 @@ class DateFormatUtilTest {
         LocalDateTime date = LocalDateTime.now().minusDays(1).minusHours(2).minusMinutes(3);
         String formatted = DateFormatUtil.formatRelativeToNow(date);
         assertThat(formatted).isEqualTo("il y a 1 jour");
+    }
+
+    @Test
+    void should_format_month_placeholders() {
+        try (MockedStatic<LocalDate> mockedStatic = Mockito.mockStatic(LocalDate.class, Mockito.CALLS_REAL_METHODS)) {
+            LocalDate testDate = LocalDate.of(2025, 4, 28);
+            mockedStatic.when(LocalDate::now).thenReturn(testDate);
+            String message = "Les mois de {mois}, {moisN-1}, {moisN-2} et {moisN-3}";
+            String formatted = DateFormatUtil.replaceMonthPlaceholder(message);
+            assertThat(formatted).isEqualTo("Les mois de Avril, Mars, Février et Janvier");
+        }
+    }
+
+    @Test
+    void should_format_month_placeholders_at_start_of_month() {
+        try (MockedStatic<LocalDate> mockedStatic = Mockito.mockStatic(LocalDate.class, Mockito.CALLS_REAL_METHODS)) {
+            LocalDate testDate = LocalDate.of(2025, 4, 10);
+            mockedStatic.when(LocalDate::now).thenReturn(testDate);
+            String message = "Les mois de {mois}, {moisN-1}, {moisN-2} et {moisN-3}";
+            String formatted = DateFormatUtil.replaceMonthPlaceholder(message);
+            assertThat(formatted).isEqualTo("Les mois de Mars, Février, Janvier et Décembre");
+        }
+    }
+
+    @Test
+    void should_ignore_invalid_placeholders() {
+        String message = "Les mois de {moisn-1}, {autre}";
+        String formatted = DateFormatUtil.replaceMonthPlaceholder(message);
+        assertThat(formatted).isEqualTo(message);
     }
 
 }
