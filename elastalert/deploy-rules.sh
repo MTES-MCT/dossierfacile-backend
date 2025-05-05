a76ea9881e8a6e40f1d60048e0c843215b22ccce
nicolasSagon <sagon.nicolas@gmail.com>
2025-05-05 14:37:46 +0200

    feature(alert): add custom alerts in python

--- elastalert/deploy-rules.sh
+++ elastalert/deploy-rules.sh
@@ -2,7 +2,8 @@
 
 # Supprimer le dossier dist s'il existe déjà, puis le recréer
 rm -rf dist
-mkdir -p dist
+mkdir -p dist/custom-alerts
+mkdir -p dist/elastalert
 
 # Fonction pour afficher l'aide
 usage() {
@@ -19,9 +20,13 @@ build() {
 
   for file in rules/*; do
     if [ -f "$file" ]; then
-      perl -pe "s/{{mattermost_webhook_url}}/$(printf '%s' "$MATTERMOST_URL" | sed 's/[&/\]/\\&/g')/g" "$file" > "dist/$(basename "$file")"
+      perl -pe "s/{{mattermost_webhook_url}}/$(printf '%s' "$MATTERMOST_URL" | sed 's/[&/\]/\\&/g')/g" "$file" > "dist/elastalert/$(basename "$file")"
     fi
   done
+
+  echo "Création d'une archive zip du dossier custom-alerts ..."
+  (cd custom-alerts && zip -r ../dist/custom-alerts/custom-alerts.zip . -x ".env")
+  pwd
 
   echo "Build terminé. Les règles ont été compilées."
 }
@@ -56,10 +61,28 @@ deploy() {
   ssh "$SSH_USERNAME@$SERVER_IP" "rm -rf /home/$SSH_USERNAME/docker-elk/elastalert/rules/*"
 
   echo "Transfert des nouveaux fichiers de règles..."
-  scp dist/* "$SSH_USERNAME@$SERVER_IP:/home/$SSH_USERNAME/docker-elk/elastalert/rules/"
+  scp dist/elastalert/* "$SSH_USERNAME@$SERVER_IP:/home/$SSH_USERNAME/docker-elk/elastalert/rules/"
+
+  echo "Suppression de l'ancienne version de custom-alerts..."
+  ssh "$SSH_USERNAME@$SERVER_IP" "rm -rf /home/$SSH_USERNAME/custom-alerts/current"
+
+  echo "Transfert du projet custom-alerts..."
+  scp dist/custom-alerts/custom-alerts.zip "$SSH_USERNAME@$SERVER_IP:/home/$SSH_USERNAME/custom-alerts"
+
+  echo "Décompression de custom-alerts..."
+  ssh "$SSH_USERNAME@$SERVER_IP" "unzip /home/$SSH_USERNAME/custom-alerts/custom-alerts.zip -d /home/$SSH_USERNAME/custom-alerts/current"
+
+  echo "Suppression de l'archive zip de custom-alerts..."
+  ssh "$SSH_USERNAME@$SERVER_IP" "rm -rf /home/$SSH_USERNAME/custom-alerts/custom-alerts.zip"
+
+  echo "Création d'un lien symbolique pour le fichier .env"
+  ssh "$SSH_USERNAME@$SERVER_IP" "ln -s /home/$SSH_USERNAME/custom-alerts/.env /home/$SSH_USERNAME/custom-alerts/current/.env"
 
   echo "Redémarrage du service ElastAlert..."
   ssh "$SSH_USERNAME@$SERVER_IP" "cd /home/$SSH_USERNAME/docker-elk && sudo docker-compose -f docker-compose-elastalert.yml up -d"
+
+  echo "Installation des dépendances python..."
+  ssh "$SSH_USERNAME@$SERVER_IP" "cd /home/$SSH_USERNAME/custom-alerts && source venv/bin/activate && cd current && pip install -r requirements.txt"
 
   echo "Déploiement terminé..."
 }
