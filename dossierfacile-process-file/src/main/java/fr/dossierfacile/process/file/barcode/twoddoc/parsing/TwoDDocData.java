ee30eff85235f93f2ba2d800894e174e8949dc6c
Jean-Marie Poissonnier <jean-marie@dossierfacile.fr>
2025-07-29 11:10:27 +0200

    ✨ Feat(processfile): Handle new 2d-doc format (#1020)
    - Add fields 4X and 4Y
    - Don't crash on unknown fields
    - Update ants tsl uri (new certificate authority FR06)

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/parsing/TwoDDocData.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/parsing/TwoDDocData.java
@@ -1,6 +1,7 @@
 package fr.dossierfacile.process.file.barcode.twoddoc.parsing;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
@@ -11,6 +12,7 @@ import fr.dossierfacile.process.file.util.TwoDDocUtil;
 
 import static fr.dossierfacile.process.file.barcode.twoddoc.parsing.TwoDDocC40Parser.ASCII_GROUP_SEPARATOR;
 
+@Slf4j
 public record TwoDDocData(Map<TwoDDocDataType, String> data) {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -19,13 +21,17 @@ public record TwoDDocData(Map<TwoDDocDataType, String> data) {
         Map<TwoDDocDataType, String> parsedData = new HashMap<>();
 
         while (reader.charactersRemaining()) {
-            TwoDDocDataType dataType = reader.readDataId();
-            String value = reader.readDataValue(dataType);
-            if (dataType.equals(TwoDDocDataType.ID_53) || dataType.equals(TwoDDocDataType.ID_54)) {
-                value = TwoDDocUtil.getLocalDateFrom2DDocHexDate(value)
-                        .format(DATE_TIME_FORMATTER);
+            try {
+                TwoDDocDataType dataType = reader.readDataId();
+                String value = reader.readDataValue(dataType);
+                if (dataType.equals(TwoDDocDataType.ID_53) || dataType.equals(TwoDDocDataType.ID_54)) {
+                    value = TwoDDocUtil.getLocalDateFrom2DDocHexDate(value).format(DATE_TIME_FORMATTER);
+                }
+                parsedData.put(dataType, value);
+            } catch (IllegalArgumentException e) {
+                // Expected error: no enum constant TwoDDocDataType.ID_XX
+                log.warn("Error parsing 2D-DOC", e);
             }
-            parsedData.put(dataType, value);
         }
 
         return new TwoDDocData(parsedData);
--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/parsing/TwoDDocDataType.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/parsing/TwoDDocDataType.java
@@ -79,6 +79,8 @@ public enum TwoDDocDataType {
     ID_49("49", 13, 13, "Numéro fiscal du déclarant 2"),
     ID_4A("4A", 8, 8, "Date de mise en recouvrement"),
     ID_4B("4B", 8, 8, "Date de la déclaration"),
+    ID_4X("4X", 0, -1, "Retenue à la source"),
+    ID_4Y("4Y", 0, -1, "Adresse"),
     ID_50("50", 14, 14, "SIRET de l’employeur"),
     ID_51("51", 6, 6, "Nombre d’heures travaillées"),
     ID_52("52", 7, 7, "Cumul du nombre d’heures travaillées"),
