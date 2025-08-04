9538a51065b7433cc92630fb828940dc01382d8d
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-04 15:36:55 +0200

    chore: add bucket encryption for new S3 provider

--- dossierfacile-pdf-generator/src/main/java/fr/dossierfacile/api/pdfgenerator/service/PdfGeneratorServiceImpl.java
+++ dossierfacile-pdf-generator/src/main/java/fr/dossierfacile/api/pdfgenerator/service/PdfGeneratorServiceImpl.java
@@ -1,6 +1,7 @@
 package fr.dossierfacile.api.pdfgenerator.service;
 
 import fr.dossierfacile.api.pdfgenerator.model.FileInputStream;
+import fr.dossierfacile.api.pdfgenerator.repository.DocumentRepository;
 import fr.dossierfacile.api.pdfgenerator.repository.FileRepository;
 import fr.dossierfacile.api.pdfgenerator.service.interfaces.PdfGeneratorService;
 import fr.dossierfacile.api.pdfgenerator.service.templates.ApartmentSharingPdfDocumentTemplate;
@@ -15,6 +16,7 @@ import fr.dossierfacile.common.enums.FileStorageStatus;
 import fr.dossierfacile.common.model.S3Bucket;
 import fr.dossierfacile.common.repository.StorageFileRepository;
 import fr.dossierfacile.common.repository.WatermarkDocumentRepository;
+import fr.dossierfacile.common.service.interfaces.EncryptionKeyService;
 import fr.dossierfacile.common.service.interfaces.FileStorageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -22,6 +24,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import java.io.FileNotFoundException;
@@ -48,6 +51,8 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
     private final BOIdentificationPdfDocumentTemplate identificationPdfDocumentTemplate;
     private final ApartmentSharingPdfDocumentTemplate apartmentSharingPdfDocumentTemplate;
     private final ApartmentSharingPdfDossierFileGenerationServiceImpl pdfFileGenerationService;
+    private final EncryptionKeyService encryptionKeyService;
+    private final DocumentRepository documentRepository;
 
 
     @Value("${pdf.generation.reattempts}")
@@ -87,6 +92,7 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
                     .path("watermark/" + UUID.randomUUID())
                     .contentType(MediaType.APPLICATION_PDF_VALUE)
                     .bucket(S3Bucket.FILIGRANE)
+                    .encryptionKey(encryptionKeyService.getCurrentKey())
                     .provider(ObjectStorageProvider.S3)
                     .status(FileStorageStatus.TEMPORARY)
                     .build();
@@ -148,8 +154,9 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
     }
 
     @Override
-    public StorageFile generateBOPdfDocument(Document document) {
-        long documentId = document.getId();
+    @Transactional
+    public StorageFile generateBOPdfDocument(Long documentId) throws FileNotFoundException {
+        Document document = documentRepository.findById(documentId).orElseThrow(() -> new FileNotFoundException(documentId.toString()));
         DocumentCategory documentCategory = document.getDocumentCategory();
         String documentCategoryName = documentCategory.name();
         log.info("Generating PDF for document [" + documentCategoryName + "] with ID [" + documentId + "]");
@@ -160,6 +167,7 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
                     .name("dossierfacile-pdf-" + document.getId() + ".pdf")
                     .path(String.format("%s/%s", document.getDocumentS3PrefixPath(), UUID.randomUUID()))
                     .bucket(S3Bucket.WATERMARK_DOC)
+                    .encryptionKey(encryptionKeyService.getCurrentKey())
                     .contentType(MediaType.APPLICATION_PDF_VALUE)
                     .build();
             return fileStorageService.upload(documentInputStream, pdfFile);
@@ -179,6 +187,7 @@ public class PdfGeneratorServiceImpl implements PdfGeneratorService {
                 StorageFile storageFile = StorageFile.builder()
                         .name("dossier_" + apartmentSharingId)
                         .bucket(S3Bucket.FULL_PDF)
+                        .encryptionKey(encryptionKeyService.getCurrentKey())
                         .path(String.format("Apartment_%s/%s", apartmentSharing.getId(), UUID.randomUUID()))
                         .contentType(MediaType.APPLICATION_PDF_VALUE)
                         .build();
