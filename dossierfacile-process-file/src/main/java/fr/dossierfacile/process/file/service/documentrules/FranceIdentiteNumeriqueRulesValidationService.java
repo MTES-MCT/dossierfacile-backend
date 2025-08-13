3ae4951026bb1c62d5070af5c79dd0a874b50563
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-13 14:37:48 +0200

    feature(process-file): rework document analysis report

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/FranceIdentiteNumeriqueRulesValidationService.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/FranceIdentiteNumeriqueRulesValidationService.java
@@ -1,22 +1,13 @@
 package fr.dossierfacile.process.file.service.documentrules;
 
-import fr.dossierfacile.common.entity.Document;
-import fr.dossierfacile.common.entity.DocumentAnalysisReport;
-import fr.dossierfacile.common.entity.DocumentAnalysisStatus;
-import fr.dossierfacile.common.entity.DocumentBrokenRule;
-import fr.dossierfacile.common.entity.DocumentRule;
-import fr.dossierfacile.common.entity.File;
-import fr.dossierfacile.common.entity.FranceIdentiteApiResult;
-import fr.dossierfacile.common.entity.ParsedFileAnalysis;
-import fr.dossierfacile.common.entity.Person;
+import fr.dossierfacile.common.entity.*;
 import fr.dossierfacile.common.enums.ParsedFileAnalysisStatus;
 import fr.dossierfacile.common.enums.ParsedFileClassification;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Optional;
 
 import static fr.dossierfacile.common.enums.DocumentSubCategory.FRANCE_IDENTITE;
@@ -34,7 +25,6 @@ public class FranceIdentiteNumeriqueRulesValidationService implements RulesValid
 
     @Override
     public DocumentAnalysisReport process(Document document, DocumentAnalysisReport report) {
-        boolean parsingOk = false;
         for (File dfFile : document.getFiles()) {
             ParsedFileAnalysis analysis = dfFile.getParsedFileAnalysis();
             if (analysis == null || analysis.getAnalysisStatus() == ParsedFileAnalysisStatus.FAILED) {
@@ -50,37 +40,42 @@ public class FranceIdentiteNumeriqueRulesValidationService implements RulesValid
                         || parsedDocument.getAttributes().getFamilyName() == null
                         || parsedDocument.getAttributes().getGivenName() == null
                         || parsedDocument.getAttributes().getValidityDate() == null) {
-                    report.addDocumentBrokenRule(DocumentBrokenRule.of(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-                    continue;
+                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
+
+                } else {
+                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
                 }
 
-                // Fake Rule
-                if (!("VALID".equals(parsedDocument.getStatus()))) {
-                    report.addDocumentBrokenRule(DocumentBrokenRule.of(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-                    continue;
+                // Fake rule
+                if (parsedDocument != null && parsedDocument.getStatus() != null && parsedDocument.getStatus().equals("VALID")) {
+                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
+                } else {
+                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
                 }
 
                 // TODO : check that France Identité verifies that names on pdf matches qrcode
                 Person documentOwner = Optional.ofNullable((Person) document.getTenant()).orElseGet(document::getGuarantor);
                 String firstName = documentOwner.getFirstName();
-                String lastName = documentOwner.getLastName();
-                if (!(normalizeName(parsedDocument.getAttributes().getGivenName()).contains(normalizeName(firstName))
-                        && (normalizeName(parsedDocument.getAttributes().getFamilyName()).contains(normalizeName(lastName)))
-                )) {
-                    log.error("Le nom/prenom ne correpond pas à l'utilisateur tenantId:" + document.getTenant().getId() + " firstname: " + firstName);
-                    report.addDocumentBrokenRule(DocumentBrokenRule.of(DocumentRule.R_FRANCE_IDENTITE_NAMES));
+
+                if (checkNamesRule(parsedDocument, document)) {
+                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_NAMES));
+                } else {
+                    log.info("Le nom/prenom ne correpond pas à l'utilisateur tenantId:{} firstname: {}", document.getTenant().getId(), firstName);
+                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_NAMES));
                 }
-                parsingOk = true;
             }
         }
-        var brokenRules = report.getBrokenRules();
-        if (brokenRules != null && brokenRules.isEmpty() && parsingOk ) {
-            report.setAnalysisStatus(DocumentAnalysisStatus.CHECKED);
-        } else if (brokenRules != null && brokenRules.stream().anyMatch(r -> r.getRule().getLevel() == DocumentRule.Level.CRITICAL)) {
-            report.setAnalysisStatus(DocumentAnalysisStatus.DENIED);
-        } else {
-            report.setAnalysisStatus(DocumentAnalysisStatus.UNDEFINED);
-        }
         return report;
+    }
+
+    private boolean checkNamesRule(@Nullable FranceIdentiteApiResult parsedDocument, Document document) {
+        if (parsedDocument == null || parsedDocument.getAttributes() == null) {
+            return false;
+        }
+        Person documentOwner = Optional.ofNullable((Person) document.getTenant()).orElseGet(document::getGuarantor);
+        String firstName = documentOwner.getFirstName();
+        String lastName = documentOwner.getLastName();
+        return normalizeName(parsedDocument.getAttributes().getGivenName()).contains(normalizeName(firstName))
+                && normalizeName(parsedDocument.getAttributes().getFamilyName()).contains(normalizeName(lastName));
     }
 }
