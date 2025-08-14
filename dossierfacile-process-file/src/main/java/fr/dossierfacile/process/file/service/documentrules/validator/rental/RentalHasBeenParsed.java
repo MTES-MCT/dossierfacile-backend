645306db1838e5c473bf2736d08f0fad56c2b0f8
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-14 15:14:01 +0200

    feature(process-file) refactor rental receipt rules

---
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/validator/rental/RentalHasBeenParsed.java
@@ -0,0 +1,28 @@
+package fr.dossierfacile.process.file.service.documentrules.validator.rental;
+
+import fr.dossierfacile.common.entity.Document;
+import fr.dossierfacile.common.entity.DocumentRule;
+import fr.dossierfacile.process.file.service.documentrules.validator.AbstractDocumentRuleValidator;
+
+public class RentalHasBeenParsed extends AbstractDocumentRuleValidator {
+
+    @Override
+    protected boolean isBlocking() {
+        return true;
+    }
+
+    @Override
+    protected boolean isInconclusive() {
+        return true;
+    }
+
+    @Override
+    protected DocumentRule getRule() {
+        return DocumentRule.R_RENT_RECEIPT_PARSED;
+    }
+
+    @Override
+    protected boolean isValid(Document document) {
+        return document.getFiles().stream().noneMatch(f -> f.getParsedFileAnalysis() == null || f.getParsedFileAnalysis().getParsedFile() == null);
+    }
+}
