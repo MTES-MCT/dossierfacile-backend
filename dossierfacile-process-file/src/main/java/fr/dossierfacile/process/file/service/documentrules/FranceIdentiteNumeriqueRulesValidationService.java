23f1231eb50e7627e291e95c9f9501b512d695e6
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-18 15:26:51 +0200

    feature(process-file): refactor franceidentite rules

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/FranceIdentiteNumeriqueRulesValidationService.java
+++ dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/documentrules/FranceIdentiteNumeriqueRulesValidationService.java
@@ -1,22 +1,21 @@
 package fr.dossierfacile.process.file.service.documentrules;
 
-import fr.dossierfacile.common.entity.*;
-import fr.dossierfacile.common.enums.ParsedFileAnalysisStatus;
-import fr.dossierfacile.common.enums.ParsedFileClassification;
+import fr.dossierfacile.common.entity.Document;
+import fr.dossierfacile.process.file.service.documentrules.validator.AbstractDocumentRuleValidator;
+import fr.dossierfacile.process.file.service.documentrules.validator.franceIdentite.FranceIdentiteHasBeenParsed;
+import fr.dossierfacile.process.file.service.documentrules.validator.franceIdentite.FranceIdentiteRuleNamesMatch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 import static fr.dossierfacile.common.enums.DocumentSubCategory.FRANCE_IDENTITE;
-import static fr.dossierfacile.process.file.util.NameUtil.normalizeName;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class FranceIdentiteNumeriqueRulesValidationService implements RulesValidationService {
+public class FranceIdentiteNumeriqueRulesValidationService extends AbstractRulesValidationService {
 
     @Override
     public boolean shouldBeApplied(Document document) {
@@ -24,58 +23,10 @@ public class FranceIdentiteNumeriqueRulesValidationService implements RulesValid
     }
 
     @Override
-    public DocumentAnalysisReport process(Document document, DocumentAnalysisReport report) {
-        for (File dfFile : document.getFiles()) {
-            ParsedFileAnalysis analysis = dfFile.getParsedFileAnalysis();
-            if (analysis == null || analysis.getAnalysisStatus() == ParsedFileAnalysisStatus.FAILED) {
-                continue;
-            }
-            if (analysis.getClassification() == ParsedFileClassification.FRANCE_IDENTITE) {
-                FranceIdentiteApiResult parsedDocument = (FranceIdentiteApiResult) analysis.getParsedFile();
-
-                // Parse Rule
-                if (parsedDocument == null
-                        || "ERROR_FILE".equals(parsedDocument.getStatus())
-                        || parsedDocument.getStatus() == null
-                        || parsedDocument.getAttributes().getFamilyName() == null
-                        || parsedDocument.getAttributes().getGivenName() == null
-                        || parsedDocument.getAttributes().getValidityDate() == null) {
-                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-
-                } else {
-                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-                }
-
-                // Fake rule
-                if (parsedDocument != null && parsedDocument.getStatus() != null && parsedDocument.getStatus().equals("VALID")) {
-                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-                } else {
-                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_STATUS));
-                }
-
-                // TODO : check that France Identité verifies that names on pdf matches qrcode
-                Person documentOwner = Optional.ofNullable((Person) document.getTenant()).orElseGet(document::getGuarantor);
-                String firstName = documentOwner.getFirstName();
-
-                if (checkNamesRule(parsedDocument, document)) {
-                    report.addDocumentPassedRule(DocumentAnalysisRule.documentPassedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_NAMES));
-                } else {
-                    log.info("Le nom/prenom ne correpond pas à l'utilisateur tenantId:{} firstname: {}", document.getTenant().getId(), firstName);
-                    report.addDocumentFailedRule(DocumentAnalysisRule.documentFailedRuleFrom(DocumentRule.R_FRANCE_IDENTITE_NAMES));
-                }
-            }
-        }
-        return report;
-    }
-
-    private boolean checkNamesRule(@Nullable FranceIdentiteApiResult parsedDocument, Document document) {
-        if (parsedDocument == null || parsedDocument.getAttributes() == null) {
-            return false;
-        }
-        Person documentOwner = Optional.ofNullable((Person) document.getTenant()).orElseGet(document::getGuarantor);
-        String firstName = documentOwner.getFirstName();
-        String lastName = documentOwner.getLastName();
-        return normalizeName(parsedDocument.getAttributes().getGivenName()).contains(normalizeName(firstName))
-                && normalizeName(parsedDocument.getAttributes().getFamilyName()).contains(normalizeName(lastName));
+    List<AbstractDocumentRuleValidator> getDocumentRuleValidators() {
+        return List.of(
+                new FranceIdentiteHasBeenParsed(),
+                new FranceIdentiteRuleNamesMatch()
+        );
     }
 }
