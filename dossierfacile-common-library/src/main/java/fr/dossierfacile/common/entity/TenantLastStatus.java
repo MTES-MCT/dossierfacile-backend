79c7ba5a06a69a11364ebf5a477518f60447d22f
nicolasSagon <sagon.nicolas@gmail.com>
2025-08-26 16:57:09 +0200

    feature(task-scheduler): implement new version of garbage collector

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/entity/TenantLastStatus.java
@@ -0,0 +1,19 @@
+package fr.dossierfacile.common.entity;
+
+import fr.dossierfacile.common.enums.LogType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TenantLastStatus {
+    private Long tenantId;
+    private LogType status;
+    private LocalDateTime creationDate;
+}
+
+
