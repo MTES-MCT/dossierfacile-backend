1cccfb1709255afe17ddd46641592e794933b623
nicolasSagon <sagon.nicolas@gmail.com>
2025-09-09 14:59:47 +0200

    feat(bo): Initialize Role and Hierarchy and user security

---
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/security/RoleHierarchyConfig.java
@@ -0,0 +1,39 @@
+package fr.gouv.bo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+
+@Configuration
+public class RoleHierarchyConfig {
+
+    @Bean
+    RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy(String.join("\n",
+                "ROLE_ADMIN > ROLE_MANAGER",
+                "ROLE_MANAGER > ROLE_SUPPORT",
+                "ROLE_SUPPORT > ROLE_OPERATOR"
+        ));
+        return roleHierarchy;
+    }
+
+    @Bean
+    MethodSecurityExpressionHandler methodSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultMethodSecurityExpressionHandler handler = new DefaultMethodSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+    }
+
+    // Optionnel: pour les expressions web (Thymeleaf sec:authorize / authorizeHttpRequests)
+    @Bean
+    DefaultWebSecurityExpressionHandler webSecurityExpressionHandler(RoleHierarchy roleHierarchy) {
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+    }
+}
--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/enums/Role.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/enums/Role.java
@@ -1,11 +1,11 @@
 package fr.dossierfacile.common.enums;
 
 public enum Role {
-    ROLE_TENANT,
-    ROLE_OWNER,
-    ROLE_ADMIN,
-    ROLE_OPERATOR,
-    ROLE_AGENT,
-    ROLE_OPERATOR_AWAY,
-    ROLE_PARTNER
+    ROLE_TENANT, // 0
+    ROLE_OWNER, // 1
+    ROLE_ADMIN, // 2
+    ROLE_OPERATOR, // 3
+    ROLE_SUPPORT, // 4
+    ROLE_MANAGER, // 5
+    ROLE_PARTNER // 6
 }
