e0bf0daa32c78b3c31348eca974a20f92fc7ad25
nicolasSagon <sagon.nicolas@gmail.com>
2025-11-25 17:40:46 +0100

    feat: Add document ia with previsualisation on bo

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/documentIA/BarcodeModel.java
@@ -0,0 +1,30 @@
+package fr.dossierfacile.common.model.documentIA;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class BarcodeModel {
+    private BarcodePosition position;
+    private int page_number;
+    private String type;
+    @Nullable
+    private Boolean is_valid = null;
+    @JsonProperty("raw_data")
+    private Object rawData;
+    @JsonProperty("typed_data")
+    private List<GenericProperty> typedData;
+    @JsonProperty("ants_type")
+    private String antsType;
+}
