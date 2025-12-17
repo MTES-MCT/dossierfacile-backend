f078b6a78f5f3d25223cf2a425d6d92624dce783
Alix Deschamps <deschamps.alix@gmail.com>
2025-12-17 17:17:50 +0100

    fix: update ApplicationControllerTest to remove unused test

--- dossierfacile-api-tenant/src/test/java/fr/dossierfacile/api/front/controller/ApplicationControllerTest.java
+++ dossierfacile-api-tenant/src/test/java/fr/dossierfacile/api/front/controller/ApplicationControllerTest.java
@@ -48,7 +48,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @WebMvcTest(ApplicationController.class)
