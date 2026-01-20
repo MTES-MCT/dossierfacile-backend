6a7414cf4ed8d6239db4faca2560acd30c87e23a
nicolasSagon <sagon.nicolas@gmail.com>
2026-01-20 11:23:44 +0100

    feat : move document analysis to api tenant and removed from processfile

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/utils/IDocumentIdentity.java
@@ -0,0 +1,11 @@
+package fr.dossierfacile.common.utils;
+
+import java.util.List;
+
+public interface IDocumentIdentity {
+    List<String> getFirstNames();
+
+    String getLastName();
+
+    String getPreferredName();
+}
