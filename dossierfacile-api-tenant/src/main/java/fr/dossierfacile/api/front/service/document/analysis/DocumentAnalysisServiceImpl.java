552d3a68c60a11ac8773476bfac1197da1d0dcc2
nicolasSagon <sagon.nicolas@gmail.com>
2026-01-20 16:15:42 +0100

    feat: write multiMapper

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/document/analysis/DocumentAnalysisServiceImpl.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/document/analysis/DocumentAnalysisServiceImpl.java
@@ -2,38 +2,27 @@ package fr.dossierfacile.api.front.service.document.analysis;
 
 import fr.dossierfacile.api.front.repository.DocumentRepository;
 import fr.dossierfacile.api.front.service.document.analysis.rule.AbstractRulesValidationService;
-import fr.dossierfacile.api.front.service.document.analysis.rule.CarteNationalIdentiteRulesValidationService;
 import fr.dossierfacile.common.entity.Document;
 import fr.dossierfacile.common.entity.DocumentAnalysisReport;
 import fr.dossierfacile.common.entity.DocumentAnalysisStatus;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
 import fr.dossierfacile.common.repository.DocumentAnalysisReportRepository;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.EnumMap;
 import java.util.LinkedList;
 import java.util.Map;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class DocumentAnalysisServiceImpl {
 
     private final Map<DocumentSubCategory, AbstractRulesValidationService> mapOfValidators;
     private final DocumentRepository documentRepository;
     private final DocumentAnalysisReportRepository documentAnalysisReportRepository;
-
-    @Autowired
-    public DocumentAnalysisServiceImpl(
-            CarteNationalIdentiteRulesValidationService carteNationalIdentiteRulesValidationService,
-            DocumentRepository documentRepository, DocumentAnalysisReportRepository documentAnalysisReportRepository) {
-        mapOfValidators = new EnumMap<>(DocumentSubCategory.class);
-        mapOfValidators.put(DocumentSubCategory.FRENCH_IDENTITY_CARD, carteNationalIdentiteRulesValidationService);
-        this.documentRepository = documentRepository;
-        this.documentAnalysisReportRepository = documentAnalysisReportRepository;
-    }
 
     public void analyseDocument(Document document) {
         var validator = mapOfValidators.get(document.getDocumentSubCategory());
@@ -41,12 +30,23 @@ public class DocumentAnalysisServiceImpl {
             log.info("Analyzing document of subcategory: {}", document.getDocumentSubCategory());
             var report = getDocumentReport(document);
             report = validator.process(document, report);
-            if (report.getFailedRules().isEmpty() && report.getInconclusiveRules().isEmpty()) {
-                report.setAnalysisStatus(DocumentAnalysisStatus.CHECKED);
-            } else if (!report.getFailedRules().isEmpty()) {
-                report.setAnalysisStatus(DocumentAnalysisStatus.DENIED);
-            } else {
+
+            boolean hasFailed = !report.getFailedRules().isEmpty();
+            boolean hasInconclusive = !report.getInconclusiveRules().isEmpty();
+            boolean hasPassed = !report.getPassedRules().isEmpty();
+
+            // Empty set -> undefined
+            if (!hasFailed && !hasInconclusive && !hasPassed) {
                 report.setAnalysisStatus(DocumentAnalysisStatus.UNDEFINED);
+            // Any failed -> denied
+            } else if (hasFailed) {
+                report.setAnalysisStatus(DocumentAnalysisStatus.DENIED);
+            // No failed, but some inconclusive -> undefined
+            } else if (hasInconclusive) {
+                report.setAnalysisStatus(DocumentAnalysisStatus.UNDEFINED);
+            // Only passed -> checked
+            } else {
+                report.setAnalysisStatus(DocumentAnalysisStatus.CHECKED);
             }
             documentAnalysisReportRepository.save(report);
             document.setDocumentAnalysisReport(report);
