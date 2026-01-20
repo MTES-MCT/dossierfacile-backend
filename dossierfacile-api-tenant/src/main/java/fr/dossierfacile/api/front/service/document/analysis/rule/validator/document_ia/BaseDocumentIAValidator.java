57d65753269bf378071affd61cca4f5984eff6e0
nicolasSagon <sagon.nicolas@gmail.com>
2026-01-20 11:23:44 +0100

    feat : move document analysis to api tenant and removed from processfile

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/document/analysis/rule/validator/document_ia/BaseDocumentIAValidator.java
+++ dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/document/analysis/rule/validator/document_ia/BaseDocumentIAValidator.java
@@ -1,10 +1,10 @@
-package fr.dossierfacile.process.file.service.document_rules.validator.document_ia;
+package fr.dossierfacile.api.front.service.document.analysis.rule.validator.document_ia;
 
+import fr.dossierfacile.api.front.service.document.analysis.rule.validator.AbstractDocumentRuleValidator;
 import fr.dossierfacile.common.entity.Document;
 import fr.dossierfacile.common.entity.DocumentIAFileAnalysis;
 import fr.dossierfacile.common.entity.File;
 import fr.dossierfacile.common.enums.DocumentIAFileAnalysisStatus;
-import fr.dossierfacile.process.file.service.document_rules.validator.AbstractDocumentRuleValidator;
 
 import java.util.List;
 import java.util.Objects;
