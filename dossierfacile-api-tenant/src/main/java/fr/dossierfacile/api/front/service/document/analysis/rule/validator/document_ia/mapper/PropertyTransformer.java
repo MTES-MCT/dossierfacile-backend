9d9e1ae7ce0b49922e56455d179a7712395331cf
nicolasSagon <sagon.nicolas@gmail.com>
2026-01-27 15:29:40 +0100

    feat(task-scheduler) : Add a new task to synchronise document ia analyse

--- dossierfacile-api-tenant/src/main/java/fr/dossierfacile/api/front/service/document/analysis/rule/validator/document_ia/mapper/PropertyTransformer.java
+++
@@ -1,6 +0,0 @@
-package fr.dossierfacile.api.front.service.document.analysis.rule.validator.document_ia.mapper;
-
-@FunctionalInterface
-public interface PropertyTransformer<I, O> {
-    O transform(I input);
-}
