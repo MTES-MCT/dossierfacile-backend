16068cabcb14e03590166ed38554d2947b3e548e
Alix Deschamps <deschamps.alix@gmail.com>
2026-02-04 12:06:04 +0100

    feat(tenant): add endpoint for document analysis status

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/repository/DocumentAnalysisReportRepository.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/repository/DocumentAnalysisReportRepository.java
@@ -3,5 +3,10 @@ package fr.dossierfacile.common.repository;
 import fr.dossierfacile.common.entity.DocumentAnalysisReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DocumentAnalysisReportRepository extends JpaRepository<DocumentAnalysisReport, Long> {
+
+    Optional<DocumentAnalysisReport> findByDocumentId(Long documentId);
+
 }
