d806dffbfbe1d08d0a08cbf2d2cf32b608fe7bf9
nicolasSagon <sagon.nicolas@gmail.com>
2026-03-03 18:11:59 +0100

    feat (process-file): Remove process file and migrate minify and metadata

--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/barcode/twoddoc/parsing/TwoDDocParsingException.java
+++
@@ -1,9 +0,0 @@
-package fr.dossierfacile.process.file.barcode.twoddoc.parsing;
-
-public class TwoDDocParsingException extends RuntimeException {
-
-	public TwoDDocParsingException(String message) {
-		super(message);
-	}
-
-}
--- dossierfacile-process-file/src/main/java/fr/dossierfacile/process/file/service/qrcodeanalysis/AuthenticationRequest.java
+++
@@ -1,4 +0,0 @@
-package fr.dossierfacile.process.file.service.qrcodeanalysis;
-
-public interface AuthenticationRequest {
-}
