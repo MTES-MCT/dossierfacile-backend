204a992a8497d10f3bba4014ac204a4918c5140b
Romaric Haldenwang <romaric@dossierfacile.fr>
2026-03-09 16:42:33 +0100

    feat(api-tenant/bo): add rate limiting filters for public and authenticated routes

--- dossierfacile-bo/src/main/java/fr/gouv/bo/BOApplication.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/BOApplication.java
@@ -3,6 +3,7 @@ package fr.gouv.bo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -20,6 +21,7 @@ import java.lang.management.MemoryMXBean;
 @EnableJpaRepositories(basePackages = {"fr.gouv", "fr.dossierfacile"})
 @EnableJpaAuditing
 @ComponentScan(basePackages = {"fr.gouv", "fr.dossierfacile"})
+@ServletComponentScan(basePackages = {"fr.gouv", "fr.dossierfacile"})
 @EnableScheduling
 @EnableAsync
 @Slf4j
