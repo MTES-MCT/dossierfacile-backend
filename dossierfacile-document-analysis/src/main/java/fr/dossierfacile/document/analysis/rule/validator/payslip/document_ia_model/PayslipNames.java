a892aac1a62042e02cb656a5f8d1b58ccfbac67e
nicolasSagon <sagon.nicolas@gmail.com>
2026-03-09 10:21:32 +0100

    feat: remove type from documentIA field annotation

--- dossierfacile-document-analysis/src/main/java/fr/dossierfacile/document/analysis/rule/validator/payslip/document_ia_model/PayslipNames.java
+++ dossierfacile-document-analysis/src/main/java/fr/dossierfacile/document/analysis/rule/validator/payslip/document_ia_model/PayslipNames.java
@@ -3,7 +3,6 @@ package fr.dossierfacile.document.analysis.rule.validator.payslip.document_ia_mo
 import fr.dossierfacile.common.enums.DocumentCategory;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
 import fr.dossierfacile.common.utils.IDocumentIdentity;
-import fr.dossierfacile.document.analysis.rule.validator.document_ia.DocumentIAPropertyType;
 import fr.dossierfacile.document.analysis.rule.validator.document_ia.mapper.DocumentIAField;
 import fr.dossierfacile.document.analysis.rule.validator.document_ia.mapper.DocumentIAModel;
 import lombok.Setter;
@@ -15,14 +14,12 @@ import java.util.List;
 public class PayslipNames implements IDocumentIdentity {
 
     @DocumentIAField(
-            extractionName = "nom_salarie",
-            type = DocumentIAPropertyType.STRING
+            extractionName = "nom_salarie"
     )
     public String lastName;
 
     @DocumentIAField(
-            extractionName = "prenom_salarie",
-            type = DocumentIAPropertyType.STRING
+            extractionName = "prenom_salarie"
     )
     public String firstName;
 
--- dossierfacile-document-analysis/src/test/java/fr/dossierfacile/document/analysis/rule/validator/document_ia_model/TestIdentityNestedCollectionsModel.java
+++ dossierfacile-document-analysis/src/test/java/fr/dossierfacile/document/analysis/rule/validator/document_ia_model/TestIdentityNestedCollectionsModel.java
@@ -2,7 +2,6 @@ package fr.dossierfacile.document.analysis.rule.validator.document_ia_model;
 
 import fr.dossierfacile.common.enums.DocumentCategory;
 import fr.dossierfacile.common.enums.DocumentSubCategory;
-import fr.dossierfacile.document.analysis.rule.validator.document_ia.DocumentIAPropertyType;
 import fr.dossierfacile.document.analysis.rule.validator.document_ia.mapper.DocumentIAField;
 import fr.dossierfacile.document.analysis.rule.validator.document_ia.mapper.DocumentIAModel;
 
@@ -14,25 +13,25 @@ import java.util.List;
 )
 public class TestIdentityNestedCollectionsModel {
 
-    @DocumentIAField(extractionName = "tags", type = DocumentIAPropertyType.LIST_STRING)
+    @DocumentIAField(extractionName = "tags")
     private String[] tags;
 
-    @DocumentIAField(extractionName = "holder", type = DocumentIAPropertyType.OBJECT)
+    @DocumentIAField(extractionName = "holder")
     private Holder holder;
 
-    @DocumentIAField(extractionName = "beneficiaries", type = DocumentIAPropertyType.LIST_OBJECT)
+    @DocumentIAField(extractionName = "beneficiaries")
     private List<Beneficiary> beneficiaries;
 
     static class Holder {
-        @DocumentIAField(extractionName = "first_name", type = DocumentIAPropertyType.STRING)
+        @DocumentIAField(extractionName = "first_name")
         private String firstName;
     }
 
     static class Beneficiary {
-        @DocumentIAField(extractionName = "first_name", type = DocumentIAPropertyType.STRING)
+        @DocumentIAField(extractionName = "first_name")
         private String firstName;
 
-        @DocumentIAField(extractionName = "inner_tags", type = DocumentIAPropertyType.LIST_STRING)
+        @DocumentIAField(extractionName = "inner_tags")
         private String[] innerTags;
     }
 }
