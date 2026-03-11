e959b151306cd67d1d39dc2811e44972e008f216
nicolasSagon <sagon.nicolas@gmail.com>
2026-03-11 17:54:28 +0100

    fix: rebase hell

--- dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/document_ia/BarcodePosition.java
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/document_ia/BarcodePosition.java
@@ -7,12 +7,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class BarcodePosition {
+public class BarcodePosition implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @JsonProperty("top_left")
     private int[] topLeft;
     @JsonProperty("top_right")
