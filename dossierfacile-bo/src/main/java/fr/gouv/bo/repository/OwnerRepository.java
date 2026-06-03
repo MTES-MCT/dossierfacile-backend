72e57b0ef897c06dc33d6293d32b50c38ab1dcb6
nicolasSagon <sagon.nicolas@gmail.com>
2026-06-03 11:19:19 +0200

    feat: rework owner search

--- dossierfacile-bo/src/main/java/fr/gouv/bo/repository/OwnerRepository.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/repository/OwnerRepository.java
@@ -1,22 +1,8 @@
 package fr.gouv.bo.repository;
 
 import fr.dossierfacile.common.entity.Owner;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface OwnerRepository extends JpaRepository<Owner, Long> {
-    @Query("""
-            SELECT o FROM Owner o
-            WHERE ((:email = '' AND o.email IS NULL) OR LOWER(o.email) LIKE %:email%)
-            AND ((:firstName = '' AND o.firstName IS NULL) OR LOWER(o.firstName) LIKE %:firstName%)
-            AND ((:lastName = '' AND o.lastName IS NULL) OR LOWER(o.lastName) LIKE %:lastName%)
-            """)
-    Page<Owner> searchOwners(
-            @Param("email") String email,
-            @Param("firstName") String firstName,
-            @Param("lastName") String lastName,
-            PageRequest page);
+public interface OwnerRepository extends JpaRepository<Owner, Long>, JpaSpecificationExecutor<Owner> {
 }
--- dossierfacile-bo/src/main/java/fr/gouv/bo/service/OwnerService.java
+++ dossierfacile-bo/src/main/java/fr/gouv/bo/service/OwnerService.java
@@ -2,11 +2,13 @@ package fr.gouv.bo.service;
 
 import fr.dossierfacile.common.entity.Owner;
 import fr.gouv.bo.repository.OwnerRepository;
+import fr.gouv.bo.repository.specification.OwnerSpecifications;
 import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,7 +20,12 @@ public class OwnerService {
     private final OwnerRepository ownerRepository;
 
     public Page<Owner> searchOwners(@NotNull String email, @NotNull String firstName, @NotNull String lastName, PageRequest page) {
-        return ownerRepository.searchOwners(email.toLowerCase(), firstName.toLowerCase(), lastName.toLowerCase(), page);
+        Specification<Owner> specification = Specification.unrestricted();
+        specification = specification.and(OwnerSpecifications.emailContains(email));
+        specification = specification.and(OwnerSpecifications.firstNameContains(firstName));
+        specification = specification.and(OwnerSpecifications.lastNameContains(lastName));
+
+        return ownerRepository.findAll(specification, page);
     }
 
     public Optional<Owner> findById(Long id) {
