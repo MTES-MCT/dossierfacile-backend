9ac062f09ffa894ebd18adb92abe523548f0819d
alixdeschamps <deschamps.alix@gmail.com>
2026-08-18 09:56:34 +0200

    feat: notify guarantor by email at submission, not before (#1296)

---
+++ dossierfacile-common-library/src/main/java/fr/dossierfacile/common/model/log/GuarantorNotifiedLogDetails.java
@@ -0,0 +1,17 @@
+package fr.dossierfacile.common.model.log;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class GuarantorNotifiedLogDetails {
+
+    private Long guarantorId;
+    private String email;
+
+}
